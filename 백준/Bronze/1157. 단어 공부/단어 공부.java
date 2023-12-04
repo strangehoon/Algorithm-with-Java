5d45af2c9988a0077c259b24ef345c0ae022c8cc
sanghoonLee <strangehoon2@gmail.com>
2023-12-04 23:55:52 +0900

    [Bronze I] Title: 단어 공부, Time: 152 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+ 
+		int[] arr = new int[26]; // 영문자의 개수는 26개임
+ 
+		int c = System.in.read();
+ 
+		while (c > 64) {	// 공백을 입력받는 순간 종료됨 
+			
+			if (c < 91) {
+				arr[c - 65]++;
+			} else {
+				arr[c - 97]++;
+			}
+			c = System.in.read();
+		}
+ 
+ 
+		int max = -1;
+		int ch = -2;	// ? 는 63 이다.
+ 
+		for (int i = 0; i < 26; i++) {
+ 
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = i;
+			} else if (arr[i] == max) {
+				ch = -2;
+			}
+		}
+		System.out.print((char) (ch+65));
+	}
+ 
+}
