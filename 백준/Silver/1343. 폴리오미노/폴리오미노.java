1c3fae8f0bae27b121614d10b3d0593fc0c42ef4
sanghoonLee <strangehoon2@gmail.com>
2023-12-26 20:07:50 +0900

    [Silver V] Title: 폴리오미노, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -8,49 +6,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str = br.readLine();
-        char[] arr = str.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        int cnt = 0;
+        str = str.replace("XXXX", "AAAA");
+        str = str.replace("XX", "BB");
         boolean flag = true;
-        for(int i=0; i<str.length(); i++){
-            if(arr[i]=='X'){
-                cnt++;
-            } else if (arr[i]=='.') {
-                for(int j=0; j<cnt/4; j++){
-                    sb.append("AAAA");
-                }
-                cnt %= 4;
-                for(int j=0; j<cnt/2; j++){
-                    sb.append("BB");
-                }
-                cnt %=2;
-                if(cnt!=0){
-                    flag = false;
-                    bw.write(String.valueOf(-1));
-                    break;
-                }
-                else
-                    sb.append('.');
-            }
-        }
-        if(flag==true) {
-            for (int j = 0; j < cnt / 4; j++) {
-                sb.append("AAAA");
-            }
-            cnt %= 4;
-            for (int j = 0; j < cnt / 2; j++) {
-                sb.append("BB");
-            }
-            cnt %= 2;
-            if (cnt != 0) {
+        for(char ch : str.toCharArray()){
+            if(ch =='X'){
                 flag = false;
                 bw.write(String.valueOf(-1));
-            }
-            if(flag==true){
-                bw.write(sb.toString());
+                break;
             }
         }
-
+        if(flag==true){
+            bw.write(String.valueOf(str));
+        }
         bw.flush();
         bw.close();
         br.close();
