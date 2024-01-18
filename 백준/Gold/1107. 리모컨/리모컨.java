dfbb061f79ad94087cbc83aa85b85e0b8c2384d9
sanghoonLee <strangehoon2@gmail.com>
2024-01-18 23:55:03 +0900

    [Gold V] Title: 리모컨, Time: 396 ms, Memory: 67388 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -17,47 +17,23 @@ public class Main {
                 breakdown.add(Integer.parseInt(st.nextToken()));
             }
         }
-        int cnt = 0;
-        // case 1 : +버튼 or -버튼
-        int result = Math.abs(N-100);
-        while (cnt<result){
-            // case 2 : 리모콘 채널 입력 -> -버튼
-            int plus = N+cnt;
-            boolean flag1 = true;
-            String plusString = String.valueOf(plus);
-            for(int i=0; i<plusString.length(); i++){
-                int tem = plusString.charAt(i)-'0';
-                if(breakdown.contains(tem)){
-                    flag1 = false;
+        int cnt = Math.abs(N-100);
+        for(int i=0; i<=1000000; i++){
+            String tem = String.valueOf(i);
+            boolean flag = true;
+            for(int j=0; j<tem.length(); j++){
+                if(breakdown.contains(tem.charAt(j)-'0')){
+                    flag=false;
+                    break;
                 }
             }
-            if(flag1==true) {
-                result = Math.min(result, cnt + plusString.length());
+            if(flag==true){
+                int x = Math.abs(N-i)+tem.length();
+                cnt = Math.min(cnt, x);
             }
-            // case 3 : 리모콘 채널 입력 -> +버튼
-            int minus = N-cnt;
-            boolean flag2 = true;
-            if(minus<0){
-                cnt++;
-                continue;
-            }
-            String minusString = String.valueOf(minus);
-            for (int i = 0; i < minusString.length(); i++) {
-                int tem = minusString.charAt(i) - '0';
-                if (breakdown.contains(tem)) {
-                    flag2 = false;
-                }
-            }
-            if (flag2 == true) {
-                result = Math.min(result, cnt + minusString.length());
-            }
-
-            if(flag1==true || flag2==true){
-                break;
-            }
-            cnt++;
         }
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(cnt));
+
         bw.flush();
         bw.close();
         br.close();
