2919c34f1a2dab80d47fa6a65b1d6a912fcfb22a
sanghoonLee <strangehoon2@gmail.com>
2024-01-19 18:17:03 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 496 ms, Memory: 37128 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     static int N, cnt;
     static int[][] graph;
-    static boolean[][] visited;
     public static void DFS(int jx, int jy, int ix, int iy){
         if(ix==N-1 && iy==N-1){
             cnt++;
@@ -28,37 +27,17 @@ public class Main {
     }
 
     public static void goWidth(int jx, int jy, int ix, int iy){
-        if(iy+1<N && visited[ix][iy+1]==false){
-            if(graph[ix][iy+1]==0) {
-                visited[ix][iy + 1] = true;
-                DFS(ix, iy, ix, iy + 1);
-                visited[ix][iy + 1] = false;
-            }
-        }
+        if(iy+1<N && graph[ix][iy+1]==0)
+            DFS(ix, iy, ix, iy + 1);
     }
     public static void goLength(int jx, int jy, int ix, int iy){
-        if(ix+1<N && visited[ix+1][iy]==false){
-            if(graph[ix+1][iy]==0) {
-                visited[ix + 1][iy] = true;
-                DFS(ix, iy, ix + 1, iy);
-                visited[ix + 1][iy] = false;
-            }
-        }
+        if(ix+1<N && graph[ix+1][iy]==0)
+            DFS(ix, iy, ix + 1, iy);
     }
     public static void goDiagonal(int jx, int jy, int ix, int iy){
-        if(iy+1<N && ix+1<N){
-            if(visited[ix][iy+1]==false && visited[ix+1][iy]==false && visited[ix+1][iy+1]==false){
-                if(graph[ix][iy+1]==0 && graph[ix+1][iy]==0 && graph[ix+1][iy+1]==0){
-                    visited[ix][iy+1]=true;
-                    visited[ix+1][iy]=true;
-                    visited[ix+1][iy+1]=true;
-                    DFS(ix,iy,ix+1, iy+1);
-                    visited[ix][iy+1]=false;
-                    visited[ix+1][iy]=false;
-                    visited[ix+1][iy+1]=false;
-                }
-            }
-        }
+        if(iy+1<N && ix+1<N)
+            if(graph[ix][iy+1]==0 && graph[ix+1][iy]==0 && graph[ix+1][iy+1]==0)
+                DFS(ix,iy,ix+1, iy+1);
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -66,15 +45,12 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         graph = new int[N][N];
-        visited = new boolean[N][N];
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++){
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        visited[0][0] = true;
-        visited[0][1] = true;
         cnt = 0;
         DFS(0,0, 0,1);
         bw.write(String.valueOf(cnt));
