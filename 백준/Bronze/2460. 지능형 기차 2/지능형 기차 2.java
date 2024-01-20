c980b3063029d0c95adde6d668fce6ac19325034
sanghoonLee <strangehoon2@gmail.com>
2024-01-20 15:18:29 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -5,14 +5,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int init_get_out = Integer.parseInt(st.nextToken());
-        int init_get_in = Integer.parseInt(st.nextToken());
-        int max_value = init_get_in-init_get_out;
-        int sum = init_get_in-init_get_out;
-        int cnt = 9;
+        int cnt = 10;
+        int sum = 0;
+        int max_value = Integer.MIN_VALUE;
         while (cnt-->0){
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int get_out = Integer.parseInt(st.nextToken());
             int get_in = Integer.parseInt(st.nextToken());
             sum += (get_in-get_out);
