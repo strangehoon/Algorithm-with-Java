91af7a9948d4174a62740f9c1b383fbb277114f2
sanghoonLee <strangehoon2@gmail.com>
2024-01-21 16:09:51 +0900

    [Silver II] Title: 베르트랑 공준, Time: 180 ms, Memory: 15428 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,19 +1,9 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        List<Integer> list = new ArrayList<>();
-        while (true){
-            int n = Integer.parseInt(br.readLine());
-            if(n==0)
-                break;
-            else
-                list.add(n);
-        }
+
         // 에라토스테네스의 체
         boolean[] check = new boolean[123456*2+1];
         check[1] = true;
@@ -22,19 +12,22 @@ public class Main {
                 check[j] = true;
             }
         }
-        
-        // 소수 카운팅 및 출력
-        for(int i=0; i<list.size(); i++){
-            int cnt = 0;
-            int x = list.get(i);
-            for(int j=x+1; j<=2*x;j++){
-                if(check[j]==false){
-                    cnt++;
+
+        while (true){
+            int n = Integer.parseInt(br.readLine());
+            if(n==0)
+                break;
+            else{
+                // 소수 카운팅 및 출력
+                int cnt = 0;
+                for(int j=n+1; j<=2*n;j++){
+                    if(check[j]==false){
+                        cnt++;
+                    }
                 }
+                bw.write(String.valueOf(cnt)+"\n");
             }
-            bw.write(String.valueOf(cnt)+"\n");
         }
-        
         bw.flush();
         bw.close();
         br.close();
