40d510c41324e0b3101cf8ccab2d0a1072abb0e9
sanghoonLee <strangehoon2@gmail.com>
2024-01-22 16:59:11 +0900

    [Gold IV] Title: 가르침, Time: 828 ms, Memory: 20924 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static String[] arr;
+    static int max_value = Integer.MIN_VALUE;
+    static int K;
+    static List<Character> alpha;
+    static List<Character> choiceAlpha;
+    public static void DFS(int pos, int cnt){
+        if(cnt==Math.min(K, alpha.size()+5)){
+            max_value = Math.max(calculate(), max_value);
+            return;
+        }
+        if(pos>alpha.size())
+            return;
+        for(int i=pos; i<alpha.size(); i++){
+            choiceAlpha.add(alpha.get(i));
+            DFS(i+1, cnt+1);
+            choiceAlpha.remove(alpha.get(i));
+        }
+    }
+
+    public static int calculate(){
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++){
+            String tem = arr[i];
+            boolean flag = true;
+            for(int j=0; j<tem.length(); j++){
+                if(!choiceAlpha.contains(tem.charAt(j))){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag==true){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        // a,n,t,i,c는 무조건 있어야함
+        if(K<5){
+            System.out.println(0);
+            return;
+        }
+
+        arr = new String[N];
+        alpha = new ArrayList<>();
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+            arr[i] = str;
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j)!='a' && str.charAt(j)!='n' && str.charAt(j)!='t' && str.charAt(j)!='i'
+                && str.charAt(j)!= 'c' && !alpha.contains(str.charAt(j))){
+                    alpha.add(str.charAt(j));
+                }
+            }
+        }
+        choiceAlpha = new ArrayList<>();
+        choiceAlpha.add('a');
+        choiceAlpha.add('n');
+        choiceAlpha.add('t');
+        choiceAlpha.add('i');
+        choiceAlpha.add('c');
+
+        DFS(0, 5);
+        System.out.println(max_value);
+    }
+}
