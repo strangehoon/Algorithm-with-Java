3b7bd871cc48ed08102f4c2f87cd32b18f294688
sanghoonLee <strangehoon2@gmail.com>
2024-01-26 16:19:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1380 ms, Memory: 72256 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int N;
+    static boolean[] visited;
+    static int[] parent;
+    static List<List<Integer>> graph;
+    public static void BFS(int root){
+        visited[root] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int tem = queue.poll();
+            for(int x : graph.get(tem)){
+                if(!visited[x]){
+                    visited[x] = true;
+                    parent[x] = tem;
+                    queue.offer(x);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N+1];
+        parent = new int[N+1];
+        graph = new ArrayList<>();
+        for(int i=0; i<=N; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i=0; i<N-1; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        BFS(1);
+        for(int i=2; i<=N; i++){
+            System.out.println(parent[i]);
+        }
+    }
+}
