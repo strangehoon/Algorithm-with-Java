2b919e61f27380994a78406979d627a609b3f2c9
sanghoonLee <strangehoon2@gmail.com>
2024-01-27 19:55:27 +0900

    [Silver III] Title: 퇴사, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] result = new int[N+1];
+        for(int i=1; i<N+1; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int T = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+
+            for(int j=0; j<i; j++){
+                result[i] = Math.max(result[j], result[i]);
+            }
+            if(i+T-1<=N){
+                result[i+T-1] = Math.max(result[i-1]+P, result[i+T-1]);
+            }
+        }
+        
+        bw.write(String.valueOf(result[N]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
