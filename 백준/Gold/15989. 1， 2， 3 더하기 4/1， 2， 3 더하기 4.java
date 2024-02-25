e4d483931a317f9f049f0144e14ad2a53aae7860
sanghoonLee <strangehoon2@gmail.com>
2024-02-25 22:27:12 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 188 ms, Memory: 16712 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        while (T-->0){
+            int n = Integer.parseInt(br.readLine());
+            for(int i=4; i<=n; i++){
+                dp[i][1] = dp[i-1][1];
+                dp[i][2] = dp[i-2][1] + dp[i-2][2];
+                dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+            }
+            bw.write(String.valueOf(dp[n][1]+dp[n][2]+dp[n][3])+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
