a0c7d998c7e76ef8ca1cdcbd101699f576ba7365
sanghoonLee <strangehoon2@gmail.com>
2024-02-28 17:00:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1020 ms, Memory: 130624 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -10,9 +10,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
         int[][] graph = new int[N+1][N+1];
-        int[][] prefix_sum = new int[N+1][N+1];
         for(int i=1; i<N+1; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=1; j<N+1; j++){
@@ -20,25 +18,23 @@ public class Main {
             }
         }
 
+        int[][] prefix_sum = new int[N+1][N+1];
         for(int i=1; i<N+1; i++){
             for(int j=1; j<N+1; j++){
-                prefix_sum[j][i] = prefix_sum[j-1][i] + graph[j][i];
+                prefix_sum[i][j] = prefix_sum[i-1][j]+prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + graph[i][j];
             }
         }
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int s_x = Integer.parseInt(st.nextToken());
-            int s_y = Integer.parseInt(st.nextToken());
-            int e_x = Integer.parseInt(st.nextToken());
-            int e_y = Integer.parseInt(st.nextToken());
-            int result = 0;
-            for(int j=s_y; j<=e_y; j++){
-                result += prefix_sum[e_x][j]-prefix_sum[s_x-1][j];
-            }
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int result = prefix_sum[x2][y2] - prefix_sum[x2][y1-1] - prefix_sum[x1-1][y2] + prefix_sum[x1-1][y1-1];
             bw.write(String.valueOf(result)+"\n");
         }
-
+        
         bw.flush();
         bw.close();
         br.close();
