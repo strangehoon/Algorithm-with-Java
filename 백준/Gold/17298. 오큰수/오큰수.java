2a6d2cc7c25c0a6ed8555b277c5d535c3f5ee066
sanghoonLee <strangehoon2@gmail.com>
2024-03-01 21:11:54 +0900

    [Gold IV] Title: 오큰수, Time: 1208 ms, Memory: 178020 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -3,52 +3,39 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static class Element{
-        int pos;
-        int value;
-        public Element(int pos, int value){
-            this.pos = pos;
-            this.value = value;
-        }
-    }
+
     public static void main(String[] args) throws IOException {
+        // input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        Stack<Integer> stack = new Stack<>();
-        Stack<Element> stack2 = new Stack<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] result = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for(int i=N-1; i>=0; i--){
-            stack.push(arr[i]);
-        }
-        int i= 0;
-
-        while (!stack.isEmpty()){
-            int tem = stack.pop();
-            if(stack2.isEmpty() || stack2.peek().value>=tem){
-                stack2.push(new Element(i, tem));
+        
+        // code
+        for(int i=0; i<N; i++){
+            if(stack.isEmpty() || arr[stack.peek()]>=arr[i]){
+                stack.push(i);
                 result[i] = -1;
             }
-            else {
-                result[i] = -1;
-                while (!stack2.isEmpty()&&stack2.peek().value<tem){
-                    Element y = stack2.pop();
-                    result[y.pos] = tem;
+            else{
+                while (!stack.isEmpty() && arr[stack.peek()]<arr[i]){
+                    int tem = stack.pop();
+                    result[tem] = arr[i];
                 }
-                stack2.push(new Element(i, tem));
+                stack.push(i);
+                result[i] = -1;
             }
-            i++;
         }
-
-        for(int j=0; j<N; j++){
-            bw.write(String.valueOf(result[j])+" ");
+        
+        // output
+        for(int i=0; i<N; i++){
+            bw.write(String.valueOf(result[i])+" ");
         }
         bw.flush();
         bw.close();
