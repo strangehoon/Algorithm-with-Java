490ccfd629983272c8dab9fb5616a0302fd425de
sanghoonLee <strangehoon2@gmail.com>
2024-03-02 15:13:29 +0900

    [Silver IV] Title: 수 찾기, Time: 640 ms, Memory: 49204 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,42 +1,38 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] Aarr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         for(int i=0; i<N; i++){
-            Aarr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(Aarr);
+        Arrays.sort(arr);
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<M; i++){
-            int x = Integer.parseInt(st.nextToken());
-            int lt = 0;
-            int rt = N-1;
-            boolean flag = true;
-            while (lt<=rt){
-                int mid = (lt+rt)/2;
-                if(x<Aarr[mid]){
-                    rt = mid-1;
-                } else if (x>Aarr[mid]) {
-                    lt = mid+1;
-                }
+            int tem = Integer.parseInt(st.nextToken());
+            int j=0;
+            int k = N-1;
+            boolean flag = false;
+            while (j<=k){
+                int mid = (j+k)/2;
+                if(arr[mid]<tem)
+                    j = mid+1;
+                else if (arr[mid]>tem) 
+                    k = mid-1;
                 else {
-                    flag = false;
+                    flag = true;
                     break;
                 }
             }
-            if(flag==false)
-                bw.write(String.valueOf(1)+"\n");
-            else
-                bw.write(String.valueOf(0)+"\n");
+            bw.write(String.valueOf(flag == false ? 0 : 1)+"\n");
         }
         bw.flush();
         bw.close();
