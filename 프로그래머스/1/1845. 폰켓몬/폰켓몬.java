108d46609c9c0e39e2da5a35b1adab9befbde0d9
sanghoonLee <strangehoon2@gmail.com>
2024-03-19 19:08:50 +0900

    [level 1] Title: 폰켓몬, Time: 3.16 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        return Math.min(nums.length/2, set.size());
+    }
+}
