8520fc270da8ca710981a12841939ffa422ab2c6
sanghoonLee <strangehoon2@gmail.com>
2024-03-23 00:53:06 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.57 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,77 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] operations) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> maxHp = new PriorityQueue<Integer>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2){
+                return o2-o1;
+            }
+        });
+        PriorityQueue<Integer> minHp = new PriorityQueue<>();
+        for(String str : operations){
+            if(str.charAt(0)=='I'){
+                Integer tem = Integer.valueOf(str.substring(2, str.length()));
+                maxHp.offer(tem);
+                minHp.offer(tem);
+                map.put(tem, map.getOrDefault(tem, 0)+1);
+            }
+            else{
+                if(str.charAt(2)=='-'){
+                    Integer a = minHp.poll();
+                    while(a!=null){
+                        if(map.get(a)==0){
+                            a = minHp.poll();
+                        }    
+                        else{
+                            map.put(a, map.get(a)-1);
+                            break;
+                        }
+                    }
+                }
+                else{
+                    Integer b = maxHp.poll();
+                    while(b!=null){
+                        if(map.get(b)==0){
+                            b = maxHp.poll();
+                        }    
+                        else{
+                            map.put(b, map.get(b)-1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        int[] result = new int[2];
+        Integer a = minHp.poll();
+        while(a!=null){
+            if(map.get(a)==0){
+                a = minHp.poll();
+            }    
+            else{
+                break;
+            }
+        }
+        if(a==null)
+            result[1] = 0;
+        else
+            result[1] = a;
+        
+        Integer b = maxHp.poll();
+        while(b!=null){
+            if(map.get(b)==0){
+                b = maxHp.poll();
+            }    
+            else{
+                break;
+            }
+        }
+        if(b==null)
+            result[0] = 0;
+        else
+            result[0] = b;
+        return result;
+    }
+}
