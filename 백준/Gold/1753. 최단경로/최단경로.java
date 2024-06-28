7b66e2b3375eaff35fc94895d9f97314aeb112f5
sanghoonLee <strangehoon2@gmail.com>
2024-06-28 19:11:47 +0900

    [Gold IV] Title: 최단경로, Time: 676 ms, Memory: 114716 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
@@ -11,61 +14,69 @@ public class Main {
             this.vertex = vertex;
             this.cost = cost;
         }
+
         @Override
-        public int compareTo(Edge edge){
-            return this.cost - edge.cost;
+        public int compareTo(Edge E) {
+            return this.cost - E.cost;
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
-        int[] dist = new int[V+1];
-        for(int i=0; i<V+1; i++){
-            dist[i] = Integer.MAX_VALUE;
-        }
+
+        int V = Integer.valueOf(st.nextToken());
+        int E = Integer.valueOf(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int K = Integer.valueOf(st.nextToken());
+
         List<List<Edge>> graph = new ArrayList<>();
         for(int i=0; i<=V; i++){
             graph.add(new ArrayList<>());
         }
 
-        for(int i=0; i<E; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            graph.get(a).add(new Edge(b, c));
+        int[] distance = new int[V+1];
+        for(int i=0; i<=V; i++){
+            distance[i] = Integer.MAX_VALUE;
         }
 
-        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(new Edge(K, 0));
-        dist[K] = 0;
-        while (!priorityQueue.isEmpty()){
-            Edge tem = priorityQueue.poll();
-            int now = tem.vertex;
-            int nowCost = tem.cost;
-            if(dist[now]<nowCost) {
+        for(int i= 0; i<E; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.valueOf(st.nextToken());
+            int v = Integer.valueOf(st.nextToken());
+            int w = Integer.valueOf(st.nextToken());
+            graph.get(u).add(new Edge(v, w));
+        }
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(K, 0));
+        distance[K] = 0;
+
+        while(!pq.isEmpty()){
+            Edge edge = pq.poll();
+            int now = edge.vertex;
+            int nowCost = edge.cost;
+
+            if(distance[now]<nowCost){
                 continue;
             }
-            for(Edge edge : graph.get(now)){
-                if(dist[edge.vertex]>nowCost+ edge.cost){
-                    dist[edge.vertex] = nowCost + edge.cost;
-                    priorityQueue.offer(new Edge(edge.vertex, dist[edge.vertex]));
+            for(Edge tem : graph.get(now)){
+                if(distance[tem.vertex]>distance[now]+tem.cost){
+                    distance[tem.vertex] = distance[now]+tem.cost;
+                    pq.offer(new Edge(tem.vertex, distance[tem.vertex]));
                 }
             }
         }
-        for(int i=1; i<V+1; i++){
-            if(dist[i]==Integer.MAX_VALUE){
-                bw.write("INF\n");
+        for(int i=1; i<=V; i++){
+            if(distance[i]==Integer.MAX_VALUE){
+                bw.write(String.valueOf("INF")+"\n");
             }
-            else
-                bw.write(String.valueOf(dist[i])+"\n");
+            else{
+                bw.write(String.valueOf(distance[i])+"\n");
+            }
         }
+
         bw.flush();
         bw.close();
         br.close();
