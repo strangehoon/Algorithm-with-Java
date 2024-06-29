47b03b22939f888c4984f3a3ec9e844f03d647b5
sanghoonLee <strangehoon2@gmail.com>
2024-06-29 20:03:07 +0900

    [Gold III] Title: 게임 개발, Time: 256 ms, Memory: 23156 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,73 +1,60 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
- 
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<ArrayList<Integer>> a = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.valueOf(st.nextToken());
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] time = new int[N+1];
+        int[] arr = new int[N+1];
+        for(int i=0; i<=N; i++){
+            graph.add(new ArrayList<>());
         }
-        int[] indegree = new int[N + 1];
-        int[] times = new int[N + 1]; 
- 
-        for (int i = 1; i <= N; i++) {
+
+        for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
- 
-            times[i] = Integer.parseInt(st.nextToken());
-            while (true) {
-                int num = Integer.parseInt(st.nextToken());
- 
-                if (num == -1) {
+            time[i] = Integer.valueOf(st.nextToken());
+            while(true){
+                int num = Integer.valueOf(st.nextToken());
+                if(num==-1)
                     break;
-                }
- 
-                a.get(num).add(i);
- 
-                indegree[i]++;
+                graph.get(num).add(i);
+                arr[i] += 1;
             }
         }
-        String ans = resolve(a, indegree, times, N);
 
-        bw.write(ans + "\n");
+        Queue<Integer> queue = new LinkedList<>();
+        int[] result = new int[N+1];
+        for(int i=1; i<=N; i++){
+            if(arr[i]==0) {
+                queue.offer(i);
+                result[i] = time[i];
+            }
+        }
+        while(!queue.isEmpty()){
+            int tem = queue.poll();
+            for(int x : graph.get(tem)){
+                result[x] = Math.max(result[x], result[tem]+time[x]);
+                arr[x] -= 1;
+                if(arr[x]==0){
+                    queue.offer(x);
+                }
+            }
+        }
+
+        for(int i=1; i<=N; i++){
+            bw.write(String.valueOf(result[i])+"\n");
+        }
         bw.flush();
         bw.close();
         br.close();
-    }
-    public static String resolve(ArrayList<ArrayList<Integer>> a, int[] indegree, int[] times, int N) {
-        Queue<Integer> q = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
-      
-        for (int i = 1; i <= N; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-        int[] result = new int[N + 1];
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            for (int next : a.get(now)) {
-                indegree[next]--;
-                result[next] = Math.max(result[next], result[now] + times[now]);
-                if (indegree[next] == 0) {
-                    q.offer(next);
-                }
-            }
-        }
-        for (int i = 1; i <= N; i++) {
-            sb.append((result[i] + times[i]) + "\n");
-        }
- 
-        return sb.toString();
     }
 }
