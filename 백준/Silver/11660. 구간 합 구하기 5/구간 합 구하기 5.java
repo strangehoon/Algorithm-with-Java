258e71917601bd7aa1b41e313a85032eac982117
sanghoonLee <strangehoon2@gmail.com>
2024-07-01 03:08:51 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 816 ms, Memory: 139648 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,40 +1,44 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
         int[][] graph = new int[N+1][N+1];
-        for(int i=1; i<N+1; i++){
+        int[][] sumArr = new int[N+1][N+1];
+        for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j<N+1; j++){
-                graph[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=1; j<=N; j++){
+                int num = Integer.valueOf(st.nextToken());
+                graph[i][j] = num;
             }
         }
 
-        int[][] prefix_sum = new int[N+1][N+1];
-        for(int i=1; i<N+1; i++){
-            for(int j=1; j<N+1; j++){
-                prefix_sum[i][j] = prefix_sum[i-1][j]+prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + graph[i][j];
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j] - sumArr[i-1][j-1] + graph[i][j];
             }
         }
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int result = prefix_sum[x2][y2] - prefix_sum[x2][y1-1] - prefix_sum[x1-1][y2] + prefix_sum[x1-1][y1-1];
+            int x1 = Integer.valueOf(st.nextToken());
+            int y1 = Integer.valueOf(st.nextToken());
+            int x2 = Integer.valueOf(st.nextToken());
+            int y2 = Integer.valueOf(st.nextToken());
+            int result = sumArr[x2][y2]-sumArr[x2][y1-1]-sumArr[x1-1][y2]+sumArr[x1-1][y1-1];
             bw.write(String.valueOf(result)+"\n");
         }
-        
+
         bw.flush();
         bw.close();
         br.close();
