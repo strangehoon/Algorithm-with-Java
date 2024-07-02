837ff0a649cf795d68dfc8bf703f78bcf6d293c1
sanghoonLee <strangehoon2@gmail.com>
2024-07-02 17:06:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.89 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
+        
         Stack<Integer> stack = new Stack<>();
-        for(int x : arr){
-            if(!stack.isEmpty() && stack.peek()==x){
-                continue;
+        for(int num : arr){
+            if(stack.isEmpty() || stack.peek()!=num){
+                stack.push(num);
             }
-            stack.push(x);
         }
-        int[] result = new int[stack.size()];
-        for(int i=stack.size()-1; i>=0; i--){
+        int size = stack.size();
+        int[] result = new int[size];
+        for(int i=size-1; i>=0; i--){
             result[i] = stack.pop();
         }
+
         return result;
     }
 }
