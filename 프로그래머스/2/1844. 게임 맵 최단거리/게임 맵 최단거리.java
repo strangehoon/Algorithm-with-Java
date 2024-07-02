aa2927d20ba99981b663120d63fac394909aa878
sanghoonLee <strangehoon2@gmail.com>
2024-07-03 03:08:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.33 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,41 @@
+
 import java.util.*;
 class Solution {
-    public int[][] maps;
-    public int n, m;
+    
     public int[] dx = {0, 0, 1, -1};
     public int[] dy = {1, -1, 0, 0};
-    public class Point {
+    public class Point{
         int x;
         int y;
-        
         public Point(int x, int y){
             this.x = x;
             this.y = y;
         }
     }
-    public int BFS(int x, int y){
+    
+    public int solution(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+        int[][] dist = new int[n][m];
         Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(x, y));
-        int[][]visited = new int[n][m];
-        visited[x][y] = 1;
+        queue.offer(new Point(0, 0));
+        dist[0][0] = 1;
         while(!queue.isEmpty()){
-            Point temPt = queue.poll();
-            if(temPt.x == n-1 && temPt.y == m-1){
-                return visited[temPt.x][temPt.y];
+            Point point = queue.poll();
+            if(point.x==n-1 && point.y==m-1){
+                return dist[n-1][m-1];
             }
             for(int i=0; i<4; i++){
-                int nx = dx[i] + temPt.x;
-                int ny = dy[i] + temPt.y;
+                int nx = point.x + dx[i];
+                int ny = point.y + dy[i];
                 if(0<=nx && nx<n && 0<=ny && ny<m){
-                    if(maps[nx][ny]==1 && visited[nx][ny]==0){
-                        visited[nx][ny] = visited[temPt.x][temPt.y] +1;
+                    if(maps[nx][ny]==1 && dist[nx][ny]==0){
+                        dist[nx][ny] = dist[point.x][point.y] + 1;
                         queue.offer(new Point(nx, ny));
                     }
                 }
             }    
         }
         return -1;
-    }
-    
-    public int solution(int[][] maps) {
-        this.maps = maps;
-        n = maps.length;
-        m = maps[0].length;
-        return BFS(0, 0);
     }
 }
