d82ca96570a4b99268be5c72488935a7856bc553
sanghoonLee <strangehoon2@gmail.com>
2024-07-02 17:33:42 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,30 +1,29 @@
 import java.util.*;
 class Solution {
     public List<Integer> solution(int[] progresses, int[] speeds) {
-
+        List<Integer> answer = new LinkedList<>();
         Queue<Integer> queue = new LinkedList<>();
         for(int i=0; i<progresses.length; i++){
-            int x = (100-progresses[i])/speeds[i];
-            if((100-progresses[i])%speeds[i]!=0)
-                x++;
-            queue.offer(x);
-        }
-        List<Integer> result = new ArrayList<>();
-        while(!queue.isEmpty()){
-            int x = queue.poll();
-            int cnt = 1;
-            while(!queue.isEmpty()){
-                if(queue.peek()<=x){
-                    queue.poll();
-                    cnt++;
-                }
-                else{
-                    break;
-                }
+            int tem = (100-progresses[i])/speeds[i];
+            if((100-progresses[i])%speeds[i]!=0){
+                tem++;
             }
-            result.add(cnt);
+            queue.offer(tem);
         }
-        
-        return result;
+        int s = queue.poll();
+        int d = 1;
+        while(!queue.isEmpty()){
+            int tem = queue.poll();
+            if(s>=tem){
+                d++;
+            }
+            else{
+                answer.add(d);
+                s = tem;
+                d = 1;
+            }
+        }
+        answer.add(d);
+        return answer;
     }
 }
