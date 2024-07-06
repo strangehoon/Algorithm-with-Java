2b5b9f550b50b5e2699dbb8aae0540ac13fc9901
sanghoonLee <strangehoon2@gmail.com>
2024-07-07 00:06:41 +0900

    [level 2] Title: 전화번호 목록, Time: 183.56 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,39 +2,21 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         int len = phone_book.length;
-        HashSet<String> set1 = new HashSet<>();
-        HashSet<String> set2 = new HashSet<>();
+        boolean flag = true;
+        HashSet<String> set = new HashSet<>();
         for(int i=0; i<len; i++){
             String str = phone_book[i];
-            boolean flag = true;
-            for(int j=0; j<str.length(); j++){
-                String tem = str.substring(0, j+1);
-                if(set1.contains(tem)){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag)
-                set1.add(str);
+            set.add(str);
         }
 
-        for(int i=len-1; i>=0; i--){
+        for(int i=0; i<len; i++){
             String str = phone_book[i];
-            boolean flag = true;
-            for(int j=0; j<str.length(); j++){
-                String tem = str.substring(0, j+1);
-                if(set2.contains(tem)){
-                    flag = false;
-                    break;
+            for(int j=1; j<str.length(); j++){
+                if(set.contains(str.substring(0, j))){
+                    return false;
                 }
             }
-            if(flag)
-                set2.add(str);
         }
-        if(set1.size()==len && set2.size()==len)
-            return true;
-        else
-            return false;
-        
+        return true;
     }
 }
