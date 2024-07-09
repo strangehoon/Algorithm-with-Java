c413ec071a2182d844ed22fc7ac246a9945839e2
sanghoonLee <strangehoon2@gmail.com>
2024-07-09 20:48:24 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.41 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,84 +1,42 @@
 import java.util.*;
 class Solution {
+    
     public int[] solution(String[] operations) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Integer[] arr = new Integer[5];
-        Arrays.sort(arr, new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2){
-                return o1-o2;
-            }
-        });
-        PriorityQueue<Integer> maxHp = new PriorityQueue<Integer>(new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2){
-                return o2-o1;
-            }
-        });
-        PriorityQueue<Integer> minHp = new PriorityQueue<>();
+        int[] answer = {0 ,0};
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minusPq = new PriorityQueue<>();
+
         for(String str : operations){
             if(str.charAt(0)=='I'){
-                Integer tem = Integer.valueOf(str.substring(2, str.length()));
-                maxHp.offer(tem);
-                minHp.offer(tem);
-                map.put(tem, map.getOrDefault(tem, 0)+1);
+                String tem = str.substring(2, str.length());
+                int num = Integer.valueOf(tem);
+                pq.offer(num);
+                minusPq.offer(num);
             }
-            else{
-                if(str.charAt(2)=='-'){
-                    Integer a = minHp.poll();
-                    while(a!=null){
-                        if(map.get(a)==0){
-                            a = minHp.poll();
-                        }    
-                        else{
-                            map.put(a, map.get(a)-1);
-                            break;
-                        }
-                    }
+            else if(str.charAt(0)=='D' && str.charAt(2)=='-'){
+                if(!minusPq.isEmpty()){
+                    int num = minusPq.poll();
+                    pq.remove(num);
                 }
-                else{
-                    Integer b = maxHp.poll();
-                    while(b!=null){
-                        if(map.get(b)==0){
-                            b = maxHp.poll();
-                        }    
-                        else{
-                            map.put(b, map.get(b)-1);
-                            break;
-                        }
-                    }
+            }
+            else {
+                if(!pq.isEmpty()){
+                    int num = pq.poll();
+                    minusPq.remove(num);
                 }
             }
         }
-        
-        int[] result = new int[2];
-        Integer a = minHp.poll();
-        while(a!=null){
-            if(map.get(a)==0){
-                a = minHp.poll();
-            }    
-            else{
-                break;
-            }
+        int max_value = 0;
+        int min_value = 0;
+        if(!pq.isEmpty()){
+            max_value = pq.poll();
+            minusPq.remove(max_value);    
         }
-        if(a==null)
-            result[1] = 0;
-        else
-            result[1] = a;
-        
-        Integer b = maxHp.poll();
-        while(b!=null){
-            if(map.get(b)==0){
-                b = maxHp.poll();
-            }    
-            else{
-                break;
-            }
+        if(!minusPq.isEmpty()){
+            min_value = minusPq.poll();
         }
-        if(b==null)
-            result[0] = 0;
-        else
-            result[0] = b;
-        return result;
+        answer[0] = max_value;
+        answer[1] = min_value;
+        return answer;
     }
 }
