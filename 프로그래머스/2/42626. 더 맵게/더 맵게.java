d1e8be3748dd005876be1868c7fefb55e7cf24a5
sanghoonLee <strangehoon2@gmail.com>
2024-07-09 23:03:19 +0900

    [level 2] Title: 더 맵게, Time: 1683.48 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,27 +1,29 @@
 import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int x : scoville){
-            pq.offer(x);
+        int result = -1;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for(int num : scoville){
+            pq.offer(Long.valueOf(num));
         }
-        int result = 0;
-        while(true){
-            Integer first = pq.poll();
-            Integer second = pq.poll();
-            if(first < K && second==null){
-                result = -1;
-                break;
+        int num = 0;
+        while(pq.size()>0){
+            if(pq.size()==1){
+                if(pq.poll()>=K)
+                    result = num;
+                else
+                    break;
             }
-            else if(first >= K && second==null){
-                break;
-            }
-            else if(first >= K && second >= K){
-                break;
-            }
-            else {
-                pq.offer(first + second*2);
-                result ++;
+            else{
+                Long first = pq.poll();
+                Long second = pq.poll();
+                if(first>=K && second>=K){
+                    result = num;
+                    break;
+                }
+                else
+                    pq.offer(first+second*2);
+                num++;    
             }
         }
         return result;
