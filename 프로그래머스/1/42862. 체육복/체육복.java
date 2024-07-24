897072067600ae910a4b3efa37c948cc17795f65
sanghoonLee <strangehoon2@gmail.com>
2024-07-24 13:43:16 +0900

    [level 1] Title: 체육복, Time: 0.13 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,31 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Set<Integer> lostSet = new HashSet<>();
-        Set<Integer> reserveSet = new HashSet<>();
-        for(int num : lost)
-            lostSet.add(num);
-        
-        for(int num : reserve){
-            if(lostSet.contains(num)){
-                lostSet.remove(num);
-            }
-            else{
-                reserveSet.add(num);
-            }
-        }
-        
-        for(int num : reserveSet){
-            if(lostSet.contains(num-1)){
-                lostSet.remove(num-1);
-                reserveSet.remove(num-1);
-            }
-            else if(lostSet.contains(num+1)){
-                lostSet.remove(num+1);
-                reserveSet.remove(num+1);
-            }
-        }
-        
-        return n-lostSet.size();
+        int answer = 0;
+        return answer;
     }
 }
