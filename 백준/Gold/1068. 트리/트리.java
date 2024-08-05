f575249f4386330c07ff3b756b9539fbd9672c4b
sanghoonLee <strangehoon2@gmail.com>
2024-08-06 05:07:45 +0900

    [Gold V] Title: 트리, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,51 +1,70 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
 import java.util.*;
-public class Main {
-    static int N;
-    static boolean[] deleted;
-    static int[] parent;
 
-    public static void BFS(int root){
-        deleted[root] = true;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()){
-            int tem = queue.poll();
-            for(int i=0; i<N; i++){
-                if(parent[i]==tem){
-                    deleted[i] = true;
-                    queue.offer(i);
-                }
+public class Main {
+
+    public static List<List<Integer>> graph;
+    public static boolean[] flags;
+    public static int cnt = 0;
+
+    public static void remove(int x){
+        flags[x] = true;
+        for(int num : graph.get(x)){
+            remove(num);
+        }
+    }
+
+    public static void DFS(int x){
+        if(flags[x])
+            return;
+
+        if(graph.get(x).isEmpty()){
+            cnt++;
+        }
+        else if(graph.get(x).size()==1 && flags[graph.get(x).get(0)]){
+            cnt++;
+        }
+        else{
+            for(int num : graph.get(x)){
+                DFS(num);
             }
         }
 
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        parent = new int[N];
-        deleted = new boolean[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(st.nextToken());
+        flags = new boolean[N];
+        graph = new ArrayList<>();
         for(int i=0; i<N; i++){
-            parent[i] = Integer.parseInt(st.nextToken());
+            graph.add(new ArrayList<>());
         }
-        int deleteNum = Integer.parseInt(br.readLine());
-        BFS(deleteNum);
-        int cnt = 0;
+        int start = -1;
+        st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            if(!deleted[i]){
-                boolean flag = true;
-                for(int j=0; j<N; j++){
-                    if(deleted[j]==false && parent[j]==i){
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag == true)
-                    cnt++;
+            int num = Integer.valueOf(st.nextToken());
+            if(num==-1){
+                start = i;
+            }
+            else{
+                graph.get(num).add(i);
             }
         }
+
+        st = new StringTokenizer(br.readLine());
+        int E = Integer.valueOf(st.nextToken());
+
+        remove(E);
+        DFS(start);
+
         bw.write(String.valueOf(cnt));
         bw.flush();
         bw.close();
