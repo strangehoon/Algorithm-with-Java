9d7f384ccbe018bfda5c38a69ac8d6a888143007
strangehoon <strangehoon2@gmail.com>
2024-08-16 14:30:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 216 ms, Memory: 55576 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,30 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[N+1][K+1];
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
 
-        for(int i=1; i<N+1; i++) {
+        int[][] dp = new int[N+1][M+1];
+        for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
-            int W = Integer.parseInt(st.nextToken());
-            int V = Integer.parseInt(st.nextToken());
-            for (int j = 1; j < K + 1; j++) {
-                if (j - W >= 0) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W] + V);
-                } else
-                    dp[i][j] = dp[i - 1][j];
+            int W = Integer.valueOf(st.nextToken());
+            int V = Integer.valueOf(st.nextToken());
+            for(int j=M; j>=0; j--){
+                dp[i][j] = dp[i-1][j];
+                if(j-W>=0){
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-W]+V);
+                }
             }
         }
-        bw.write(String.valueOf(dp[N][K]));
 
+        bw.write(String.valueOf(dp[N][M]));
         bw.flush();
         bw.close();
         br.close();
