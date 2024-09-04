3f54ac7350901047210479615c636003e3e5c208
strangehoon <strangehoon2@gmail.com>
2024-09-04 20:47:00 +0900

    [level 3] Title: 기지국 설치, Time: 0.91 ms, Memory: 68.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,20 +1,31 @@
-
+import java.util.*;
 class Solution {
+    public boolean[] arr;
+    
     public int solution(int n, int[] stations, int w) {
-        int now = 1; 
-        int stationIdx = 0; 
-        int answer = 0;
-
-        while (now <= n) {
-            if(stationIdx >= stations.length || now < stations[stationIdx] - w) {
-                answer++;
-                now = now + 2 * w + 1;
-            }
-            else {
-                now = stations[stationIdx] + w + 1;
-                stationIdx++;
-            }
+        // init
+        int idx = 1;
+        int[] arr = new int[stations.length+1];
+        for(int i=0; i<stations.length; i++){
+            arr[i] = stations[i];
         }
-        return answer;
+        arr[stations.length] = n+w+1;
+        
+        // logic
+        int jdx = 0;
+        int total = 0;
+        while(jdx<arr.length){
+            int tem = arr[jdx];
+            int begin = tem-w;
+            int end = tem+w;
+            if(idx<begin){
+                total += (begin-idx)/(2*w+1);
+                if((begin-idx)%(2*w+1)!=0)
+                    total+=1;
+            }
+            idx = end+1;
+            jdx++;
+        }
+        return total;
     }
 }
