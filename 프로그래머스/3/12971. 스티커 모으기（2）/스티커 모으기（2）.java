d916a26f264193125adcb483a6289539f4122712
strangehoon <strangehoon2@gmail.com>
2024-09-05 01:44:23 +0900

    [level 3] Title: 스티커 모으기(2), Time: 13.24 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,40 +1,29 @@
 import java.util.*;
 class Solution {
     public int solution(int sticker[]) {
-        int[] dp1 = new int[sticker.length];
-        int[] dp2 = new int[sticker.length];
         
-        if(sticker.length==1){
+        int len = sticker.length;
+        if(len==1){
             return sticker[0];
         }
-        
-        if(sticker.length==2){
+        if(len==2){
             return Math.max(sticker[0], sticker[1]);
         }
         
+        int[] dp1 = new int[len];
+        int[] dp2 = new int[len];
         dp1[0] = sticker[0];
-        dp1[1] = sticker[1];
-        dp1[2] = dp1[0] + sticker[2];
-        
+        dp1[1] = Math.max(sticker[0], sticker[1]);
         dp2[0] = 0;
         dp2[1] = sticker[1];
-        dp2[2] = sticker[2];
         
-        for(int i=3; i<sticker.length; i++){
-            dp1[i] = sticker[i];
-            dp2[i] = sticker[i];
-            
-            dp1[i] += Math.max(dp1[i-2], dp1[i-3]);
-            dp2[i] += Math.max(dp2[i-2], dp2[i-3]);
+        for(int i=2; i<len; i++){
+            if(i!=len-1)
+                dp1[i] = Math.max(dp1[i-2]+sticker[i], dp1[i-1]);
+            dp2[i] = Math.max(dp2[i-2]+sticker[i], dp2[i-1]);
         }
         
-        int max_value = Integer.MIN_VALUE;
-        for(int i=0; i<sticker.length; i++){
-            max_value = Math.max(dp2[i], max_value);
-            if(i!=sticker.length-1)
-                max_value = Math.max(dp1[i], max_value);
-        }
-    
-        return max_value;
+        
+        return Math.max(dp1[len-2], dp2[len-1]);
     }
 }
