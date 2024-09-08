d3ea1bba3a61a05f3a448fa48381e0f0beb28e34
strangehoon <strangehoon2@gmail.com>
2024-09-09 05:19:46 +0900

    [level 2] Title: [1차] 캐시, Time: 37.25 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,31 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        
-        Queue<String> queue = new LinkedList<>();
-        int result = 0;
-        for(int i=0; i<cities.length; i++){
-            String city = cities[i].toUpperCase();
-            // 캐시에 X
-            if(!queue.contains(city)){
-                if(queue.size()<cacheSize){
-                    queue.offer(city);
-                }
-                else{
-                    if(!queue.isEmpty()){
-                        queue.poll();
-                        queue.offer(city);
-                    }
-                }
-                result += 5;
-            }
-            // 캐시에 O
-            else{
-                queue.remove(city);
-                queue.offer(city);
-                result += 1;
-            }
-        }
-        return result;
+        int answer = 0;
+        return answer;
     }
 }
