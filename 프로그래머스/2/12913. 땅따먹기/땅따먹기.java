825b19e97a3c4a2c07c788be28a8c86d2ed316a2
strangehoon <strangehoon2@gmail.com>
2024-09-11 03:52:30 +0900

    [level 2] Title: 땅따먹기, Time: 21.02 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        int dp[][] = new int[land.length][4];
+        dp[0] = land[0];
+        
+        for(int i=1; i<land.length; i++){
+            for(int j=0; j<4; j++){
+                for(int k=0; k<4; k++){
+                    if(j==k)
+                        continue;
+                    dp[i][j] = Math.max(dp[i-1][k], dp[i][j]);
+                }
+                dp[i][j] += land[i][j];
+            }
+        }
+        int answer = 0;
+        for(int i=0; i<4; i++){
+            answer = Math.max(answer, dp[land.length-1][i]);
+        }
+
+        return answer;
+    }
+}
