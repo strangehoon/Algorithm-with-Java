2a168bc2b4a37826ed2dd934726e602df9972833
strangehoon <strangehoon2@gmail.com>
2024-09-11 10:50:25 +0900

    [level 2] Title: [3차] n진수 게임, Time: 46.43 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] n진수 게임 - 17687 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687#qna) 
 
 ### 성능 요약
 
-메모리: 149 MB, 시간: 136.81 ms
+메모리: 91.7 MB, 시간: 46.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 11일 05:30:12
+2024년 09월 11일 10:50:23
 
 ### 문제 설명
 
--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,51 +1,11 @@
-// 4:22
 import java.util.*;
 class Solution {
-    
-    public String calculate(int n, int x){
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        while(x>=n){
-            int tem = x%n;
-            list.add(String.valueOf(tem));
-            x /= n;
-        }
-        list.add(String.valueOf(x));
-        
-        for(int i=0; i<list.size(); i++){
-            String element = list.get(i);
-            switch (element) {
-                case "10":
-                    sb.append("A");
-                    break;
-                case "11":
-                    sb.append("B");
-                    break;
-                case "12":
-                    sb.append("C");
-                    break;
-                case "13":
-                    sb.append("D");
-                    break;
-                case "14":
-                    sb.append("E");
-                    break;
-                case "15":
-                    sb.append("F");
-                    break;
-                default:
-                    sb.append(element);
-                    break;
-            }
-        }
-        return sb.reverse().toString();
-    }
-    
+
     public String solution(int n, int t, int m, int p) {
         
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<100000; i++){
-            sb.append(calculate(n, i));
+            sb.append(Integer.toString(i, n).toUpperCase());
         }
         String str = sb.toString();
         
