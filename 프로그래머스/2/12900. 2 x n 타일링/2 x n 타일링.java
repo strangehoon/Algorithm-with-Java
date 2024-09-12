e48d6738385eaeb440288f8f1f23b5740744643d
strangehoon <strangehoon2@gmail.com>
2024-09-13 03:27:25 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.91 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -4,7 +4,7 @@ class Solution {
         dp[0] = 1;
         dp[1] = 1;
         for(int i=2; i<=n; i++){
-            dp[i] = (dp[i-2] + dp[i-1])%1000000007;
+            dp[i] = dp[i-2] + dp[i-1];
         }
         
         return dp[n];
