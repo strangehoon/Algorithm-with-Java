3be1f925100744d384dc565325de282a8c330cfb
strangehoon <strangehoon2@gmail.com>
2024-10-19 01:55:42 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 124 ms, Memory: 14768 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,35 +1,37 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(br.readLine());
         int[] arr = new int[N];
         int[] dp = new int[N];
-        for(int i=0; i< N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.valueOf(st.nextToken());
         }
-        int max_value = arr[0];
-        dp[0] = arr[0];
-        for(int i=1; i<N; i++){
-            int sum = 0;
-            for(int j=0;j<i;j++){
-                if(arr[j]<arr[i]){
-                    sum = Math.max(dp[j], sum);
-                }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<i; j++){
+                if(arr[i]>arr[j])
+                    dp[i] = Math.max(dp[i], dp[j]);
             }
-            dp[i] = sum + arr[i];
-            max_value = Math.max(dp[i], max_value);
+            dp[i] += arr[i];
         }
 
+        int result = Integer.MIN_VALUE;
+        for(int i=0; i<N; i++){
+            result = Math.max(result, dp[i]);
+        }
 
-        bw.write(String.valueOf(max_value));
-
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
         br.close();
