3ddc5d73380c6aeef0514e604ce1f3239a4c08d2
strangehoon <strangehoon2@gmail.com>
2024-10-31 20:29:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 15136 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,33 +1,40 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.valueOf(br.readLine());
         int[] arr = new int[N];
         int[] dp = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.valueOf(st.nextToken());
         }
-        int max_value = 1;
         dp[0] = 1;
         for(int i=1; i<N; i++){
-            int cnt = 0;
+            int max_value = 0;
             for(int j=0; j<i; j++){
-                if(arr[j]<arr[i]){
-                    cnt = Math.max(dp[j], cnt);
+                if(arr[i]>arr[j]){
+                    max_value = Math.max(max_value, dp[j]);
                 }
             }
-            dp[i] = cnt+1;
-            max_value = Math.max(max_value, dp[i]);
+            dp[i] += max_value+1;
         }
 
-        bw.write(String.valueOf(max_value));
+        int maxResult = 0;
+        for(int i=0; i<N; i++){
+            maxResult = Math.max(maxResult, dp[i]);
+        }
+        bw.write(String.valueOf(maxResult));
+        
         bw.flush();
         bw.close();
         br.close();
