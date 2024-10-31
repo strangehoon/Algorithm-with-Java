d6b19353a38f68cd900ae7d4cff0bbde60cf8828
strangehoon <strangehoon2@gmail.com>
2024-10-31 17:03:13 +0900

    [Gold IV] Title: 트리의 지름, Time: 260 ms, Memory: 23552 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+    public static class Edge{
+        int vertex;
+        int distance;
+
+        public Edge(int vertex, int distance){
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+    }
+
+    public static List<List<Edge>> graph;
+    public static int N;
+    public static int[] distance;
+
+    public static void BFS(int x){
+        distance = new int[N+1];
+        for(int i=1; i<=N; i++){
+            distance[i] = -1;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(x);
+        distance[x] = 0;
+
+        while (!pq.isEmpty()){
+            int curV = pq.poll();
+            for(Edge edge : graph.get(curV)){
+                if(distance[edge.vertex]==-1){
+                    pq.offer(edge.vertex);
+                    distance[edge.vertex] = distance[curV] + edge.distance;
+                 }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.valueOf(br.readLine());
+        graph = new ArrayList<>();
+        for(int i=0; i<=N; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<N-1; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.valueOf(st.nextToken());
+            int b = Integer.valueOf(st.nextToken());
+            int v = Integer.valueOf(st.nextToken());
+
+            graph.get(a).add(new Edge(b, v));
+            graph.get(b).add(new Edge(a, v));
+        }
+
+        BFS(1);
+
+        int maxVertex = Integer.MIN_VALUE;
+        int maxDistance = Integer.MIN_VALUE;
+        for(int i=1; i<=N; i++){
+            if(distance[i]>maxDistance){
+                maxVertex = i;
+                maxDistance = distance[i];
+            }
+        }
+
+        BFS(maxVertex);
+        maxVertex = Integer.MIN_VALUE;
+        maxDistance = Integer.MIN_VALUE;
+        for(int i=1; i<=N; i++){
+            if(distance[i]>maxDistance){
+                maxVertex = i;
+                maxDistance = distance[i];
+            }
+        }
+
+        bw.write(String.valueOf(maxDistance));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
