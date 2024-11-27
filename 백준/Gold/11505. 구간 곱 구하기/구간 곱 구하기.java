34d7a87f70618b5dbac70eec6970c6314f5e978c
strangehoon <strangehoon2@gmail.com>
2024-11-27 21:31:13 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 472 ms, Memory: 88192 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
@@ -32,44 +35,47 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
+        int K = Integer.valueOf(st.nextToken());
 
-        int height = (int) Math.ceil(Math.log(N) / Math.log(2));
-        int size = (int) Math.pow(2, height + 1);
-        int leftNodeStartIdx = size / 2 - 1;
+        int height = 0;
+        int tem = N;
+        while (tem!=0){
+            tem /= 2;
+            height++;
+        }
+
+        int size = (int) Math.pow(2, height+1);
+        int leftNodeStartIdx = size/2-1;
         tree = new long[size];
         Arrays.fill(tree, 1);
 
-        // 리프 노드 입력
-        for (int i = leftNodeStartIdx + 1; i <= leftNodeStartIdx + N; i++) {
+        for(int i=leftNodeStartIdx+1; i<=leftNodeStartIdx+N; i++){
             tree[i] = Long.parseLong(br.readLine());
         }
 
-        // 세그먼트 트리 초기화
         for (int i = leftNodeStartIdx; i > 0; i--) {
-            tree[i] = (tree[i * 2] * tree[i * 2 + 1]) % 1000000007;
+            tree[i] = (tree[i * 2] * tree[i * 2 + 1]) % 1000000007; // 두 자식의 곱
         }
 
-        // 쿼리 처리
-        for (int i = 0; i < M + K; i++) {
+        for(int i=0; i<M+K; i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
+            long a = Integer.parseInt(st.nextToken());
             int s = Integer.parseInt(st.nextToken());
             long e = Long.parseLong(st.nextToken());
-
-            if (a == 1) { // 값 변경
-                changeValue(leftNodeStartIdx + s, e);
-            } else if (a == 2) { // 구간 곱 구하기
-                s += leftNodeStartIdx;
-                e += leftNodeStartIdx;
-                bw.write(getSum(s, (int) e) + "\n");
+            if(a==1){
+                changeValue(leftNodeStartIdx+s, e);
+            }
+            else if(a==2){
+                s = s+leftNodeStartIdx;
+                e = e+leftNodeStartIdx;
+                bw.write(String.valueOf(getSum(s, (int)e))+"\n");
             }
         }
 
