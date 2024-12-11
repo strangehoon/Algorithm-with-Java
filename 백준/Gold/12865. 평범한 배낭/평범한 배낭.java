556d794e2844bf02b31df6a302207b4153f988c0
strangehoon <strangehoon2@gmail.com>
2024-12-11 17:56:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 204 ms, Memory: 55572 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -11,23 +11,23 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.valueOf(st.nextToken());
-        int M = Integer.valueOf(st.nextToken());
+        int n = Integer.valueOf(st.nextToken());
+        int k = Integer.valueOf(st.nextToken());
+        int[][] dp = new int[n+1][k+1];
 
-        int[][] dp = new int[N+1][M+1];
-        for(int i=1; i<=N; i++){
+        for(int i=1; i<=n; i++){
             st = new StringTokenizer(br.readLine());
-            int W = Integer.valueOf(st.nextToken());
-            int V = Integer.valueOf(st.nextToken());
-            for(int j=M; j>=0; j--){
+            int w = Integer.valueOf(st.nextToken());
+            int v = Integer.valueOf(st.nextToken());
+            for(int j=0; j<=k; j++){
                 dp[i][j] = dp[i-1][j];
-                if(j-W>=0){
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-W]+V);
+                if(j-w>=0){
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-w]+v);
                 }
             }
         }
 
-        bw.write(String.valueOf(dp[N][M]));
+        bw.write(String.valueOf(dp[n][k]));
         bw.flush();
         bw.close();
         br.close();
