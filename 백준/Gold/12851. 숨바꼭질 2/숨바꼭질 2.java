ae2223ba18fd27a359edb85b4b20b96344e46370
strangehoon <strangehoon2@gmail.com>
2024-12-13 01:05:02 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 200 ms, Memory: 56796 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,77 +1,75 @@
-import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
 
-    static int N,K,time;
-    static int[]visited;
-    static int[] method;
-    static boolean flag;
+    public static class Pos{
+        int x;
+        int t;
 
-    public static void BFS(){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(N);
-        while (!queue.isEmpty()&&flag==false){
-            int size = queue.size();
-            for(int i=0; i<size; i++){
-                int tem = queue.poll();
-                if(tem-1>=0 && (visited[tem-1]==0 || visited[tem-1]==time)){
-                    if(tem-1==K){
-                        flag = true;
-                    }
-                    if(visited[tem-1]==0) {
-                        visited[tem - 1] = time;
-                        queue.offer(tem - 1);
-                    }
-                    method[tem-1]+=method[tem];
-                }
-                if (tem+1<=200000 && (visited[tem+1]==0||visited[tem+1]==time)) {
-                    if(tem+1==K){
-                        flag=true;
-                    }
-                    if(visited[tem+1]==0) {
-                        visited[tem + 1] = time;
-                        queue.offer(tem + 1);
-                    }
-                    method[tem+1]+=method[tem];
-                }
-                if (tem*2<=200000 && (visited[tem*2]==0||visited[tem*2]==time)) {
-                    if(tem*2==K){
-                        flag=true;
-                    }
-                    if(visited[tem*2]==0) {
-                        visited[tem * 2] = time;
-                        queue.offer(tem * 2);
-                    }
-                    method[tem*2]+=method[tem];
-                }
-            }
-            time++;
+        public Pos(int x, int t){
+            this.x = x;
+            this.t = t;
         }
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        visited = new int[200001];
-        method = new int[200001];
-        time = 1;
-        method[N]=1;
-        flag = false;
-        if(N==K){
-            bw.write(String.valueOf(0+"\n"));
-            bw.write(String.valueOf(1));
+        int n = Integer.valueOf(st.nextToken());
+        int k = Integer.valueOf(st.nextToken());
+
+        int[] timeArr = new int[Math.max(2*k+1, 2*n+1)];
+        Arrays.fill(timeArr, -1);
+
+        Queue<Pos> queue = new LinkedList<>();
+        queue.add(new Pos(n, 0));
+        timeArr[n] = 0;
+
+        int shortestTime = Integer.MAX_VALUE;
+        int cnt = 0;
+        while (!queue.isEmpty()) {
+            Pos nowPos = queue.poll();
+            int nowX = nowPos.x;
+            int nowT = nowPos.t;
+
+            if (nowX == k) {
+                if (shortestTime > nowT) {
+                    shortestTime = nowT;
+                    cnt = 1;
+                } else {
+                    cnt++;
+                }
+                continue;
+            }
+
+            if (nowX - 1 >= 0) {
+                if (timeArr[nowX - 1] == -1 || timeArr[nowX - 1] == nowT + 1) {
+                    timeArr[nowX - 1] = nowT + 1;
+                    queue.offer(new Pos(nowX - 1, nowT + 1));
+                }
+            }
+            if (nowX <= k - 1) {
+                if (timeArr[nowX * 2] == -1 || timeArr[nowX * 2] == nowT + 1) {
+                    timeArr[nowX * 2] = nowT + 1;
+                    queue.offer(new Pos(nowX * 2, nowT + 1));
+                }
+            }
+            if (nowX + 1 <= k) {
+                if (timeArr[nowX + 1] == -1 || timeArr[nowX + 1] == nowT + 1) {
+                    timeArr[nowX + 1] = nowT + 1;
+                    queue.offer(new Pos(nowX + 1, nowT + 1));
+                }
+            }
         }
-        else {
-            BFS();
-            bw.write(String.valueOf(visited[K] + "\n"));
-            bw.write(String.valueOf(method[K]));
-        }
+
+        bw.write(String.valueOf(shortestTime) + "\n");
+        bw.write(String.valueOf(cnt));
         bw.flush();
         bw.close();
         br.close();
