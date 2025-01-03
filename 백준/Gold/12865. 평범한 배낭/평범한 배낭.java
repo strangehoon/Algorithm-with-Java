5dc263cc692907f80cbf2249ae0630bd619d2d0a
strangehoon <strangehoon2@gmail.com>
2025-01-03 17:05:27 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 55036 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,35 +1,27 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.valueOf(st.nextToken());
-        int k = Integer.valueOf(st.nextToken());
-        int[][] dp = new int[n+1][k+1];
+        int N = Integer.valueOf(st.nextToken());
+        int K = Integer.valueOf(st.nextToken());
 
-        for(int i=1; i<=n; i++){
+        int[][] dp = new int[N+1][K+1];
+        for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
             int w = Integer.valueOf(st.nextToken());
             int v = Integer.valueOf(st.nextToken());
-            for(int j=0; j<=k; j++){
+            for(int j=1; j<=K; j++){
                 dp[i][j] = dp[i-1][j];
-                if(j-w>=0){
+                if(j-w>=0)
                     dp[i][j] = Math.max(dp[i][j], dp[i-1][j-w]+v);
-                }
             }
         }
-
-        bw.write(String.valueOf(dp[n][k]));
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(dp[N][K]);
     }
 }
