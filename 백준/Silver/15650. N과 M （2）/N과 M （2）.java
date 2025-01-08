e32636608ab1f32590b797d5d073359679b8dd0a
strangehoon <strangehoon2@gmail.com>
2025-01-08 19:58:11 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] arr;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+    public static int n,m;
+
+    public static void sol(int start, int level){
+
+        if(level==m){
+            for(int x:arr){
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<=n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[level] = i;
+                sol(i+1, level+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        visited = new boolean[n+1];
+
+        sol(1, 0);
+        System.out.println(sb.toString());
+
+    }
+}
