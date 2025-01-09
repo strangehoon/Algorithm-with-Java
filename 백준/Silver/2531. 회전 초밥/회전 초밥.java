6dacfe677e731c890a910b884933dc8bd5bba582
strangehoon <strangehoon2@gmail.com>
2025-01-10 02:08:05 +0900

    [Silver I] Title: 회전 초밥, Time: 228 ms, Memory: 22108 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,52 +1,41 @@
 import java.io.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        int result = Integer.MIN_VALUE;
+        int[] arr = new int[N];
         for(int i=0; i<N; i++){
-            int x = Integer.parseInt(br.readLine());
-            arrayList.add(x);
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
         }
 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(c, 1);
         for(int i=0; i<k; i++){
-            map.put(arrayList.get(i), map.getOrDefault(arrayList.get(i), 0)+1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
         }
-        if(map.containsKey(c))
-            result = map.size();
-        else
-            result = map.size()+1;
-        int cnt = 1;
-        int i = k;
-        int j = 0;
-        while (cnt++<N){
-            map.put(arrayList.get(i%N), map.getOrDefault(arrayList.get(i%N), 0)+1);
-            map.put(arrayList.get(j%N), map.get(arrayList.get(j%N))-1);
-            if(map.get(arrayList.get(j%N))<1){
-                map.remove(arrayList.get(j%N));
-            }
-            if(map.containsKey(c))
-                result = Math.max(result, map.size());
-            else
-                result = Math.max(result, map.size()+1);
-            i++;
-            j++;
-        }
+        int maxType = map.size();
 
-        bw.write(String.valueOf(result));
-        bw.flush();
-        bw.close();
-        br.close();
+        int idx = k;
+        for(int i=idx; i<2*N; i++){
+            // 다시 원점 돌아오면 반복문 탈출
+            if(i%N==k-1)
+                break;
+            map.put(arr[i%N], map.getOrDefault(arr[i%N], 0)+1);
+            map.put(arr[(i-k+N)%N], map.get(arr[(i-k+N)%N])-1);
+            if(map.get(arr[(i-k+N)%N])==0)
+                map.remove(arr[(i-k+N)%N]);
+            maxType = Math.max(maxType, map.size());
+        }
+        System.out.println(maxType);
     }
 }
