2724edc4fc67adbf0ae834e068e0de5263678c91
strangehoon <strangehoon2@gmail.com>
2025-02-05 01:57:28 +0900

    Update ［3차］ 방금그곡.java

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -2,71 +2,75 @@ import java.util.*;
 
 class Solution {
    
-    public int transTime(String start, String end){
-        int result = 0;
-        result += (Integer.parseInt(end.substring(0, 2)) - Integer.parseInt(start.substring(0, 2)))*60;
-        result += (Integer.parseInt(end.substring(3, 5)) - Integer.parseInt(start.substring(3, 5)));
-        return result;
+    // HH:MM 형식의 시간을 분 단위로 변환
+    public int calculateMin(String start, String end){
+        int min = 0;
+        min += (Integer.parseInt(end.substring(0, 2)) - Integer.parseInt(start.substring(0, 2)))*60;
+        min += (Integer.parseInt(end.substring(3, 5)) - Integer.parseInt(start.substring(3, 5)));
+        return min;
     }
     
-    public String transMusic(int time, String sheet){
-        StringBuilder result = new StringBuilder();
+    // 재생 시간 동안 반복된 악보 생성
+    public String generatePlayedMelody(int playTime, String sheet) {
+        String convertedSheet = convertMelody(sheet);
+        int convertedSheetLen = convertedSheet.length();
+        StringBuilder playedMelody = new StringBuilder();
         int idx = 0;
-        while(time>0){
-            char ch = sheet.charAt(idx);
-            idx = (idx+1)%sheet.length();
-            result.append(ch);
-            time--;
+        while(playTime>0){
+            char ch = convertedSheet.charAt(idx);
+            idx = (idx+1)%convertedSheetLen;
+            playedMelody.append(ch);
+            playTime--;
         }
-        return result.toString();
-    }
-
-    public boolean isCorrect(String m, String music){
-        if(music.contains(m))
-            return true;
-        else
-            return false;
+        return playedMelody.toString();
     }
     
-    public class Answer implements Comparable<Answer>{
-        int time;
+    // 기억한 멜로디가 재생된 음악에 포함되는지 확인
+    public boolean isMelodyMatched(String rememberedMelody, String playedMelody) {
+        return playedMelody.contains(rememberedMelody);
+    }
+    
+    // 음악 정보를 저장하는 클래스 (우선순위 큐 정렬을 위해 Comparable 구현)
+    public class MusicInfo implements Comparable<MusicInfo> {
+        int playTime;
         String title;
         int order;
-        
-        public Answer(int time, String title, int order){
-            this.time = time;
+
+        public MusicInfo(int playTime, String title, int order) {
+            this.playTime = playTime;
             this.title = title;
             this.order = order;
         }
+
         @Override
-        public int compareTo(Answer a){
-            if(this.time==a.time){
-                return this.order-a.order;
+        public int compareTo(MusicInfo other) {
+            if (this.playTime == other.playTime) {
+                return this.order - other.order; // 먼저 입력된 곡이 우선
             }
-            else{
-                return a.time-this.time;
-            }
+            return other.playTime - this.playTime; // 재생 시간이 긴 곡이 우선
         }
     }
-    public String replaceMusic(String str){
-        return str.replaceAll("C#", "c")
-            .replaceAll("B#", "b")
-            .replaceAll("D#", "d")
-            .replaceAll("F#", "f")
-            .replaceAll("G#", "g")
-            .replaceAll("A#", "a");
+    
+     // 멜로디를 변환 (C# -> c, D# -> d 등)
+    public String convertMelody(String melody) {
+        return melody.replaceAll("C#", "c")
+                     .replaceAll("D#", "d")
+                     .replaceAll("F#", "f")
+                     .replaceAll("G#", "g")
+                     .replaceAll("A#", "a")
+                     .replaceAll("B#", "b");
     }
     
     public String solution(String m, String[] musicinfos) {
-        PriorityQueue<Answer> pq = new PriorityQueue<>();
-        String mm = replaceMusic(m);
+        PriorityQueue<MusicInfo> pq = new PriorityQueue<>();
+        String rememberedMelody = convertMelody(m);
+        
         for(int i=0, len=musicinfos.length; i<len; i++){
             String[] musicinfoArr = musicinfos[i].split(",");
-            int time = transTime(musicinfoArr[0], musicinfoArr[1]);
-            String music = transMusic(time, replaceMusic(musicinfoArr[3]));
-            boolean flag = isCorrect(mm, music); 
-            if(flag){
-                pq.offer(new Answer(time, musicinfoArr[2], i));
+            int min = calculateMin(musicinfoArr[0], musicinfoArr[1]);
+            String playedMelody = generatePlayedMelody(min, musicinfoArr[3]);
+            if (isMelodyMatched(rememberedMelody, playedMelody)) {
+                pq.offer(new MusicInfo(min, musicinfoArr[2], i));
             }
         }
         if(pq.isEmpty())
