9856a9fa5126b909460e5fe5d8f14155ceb227b5
strangehoon <strangehoon2@gmail.com>
2025-02-11 20:38:06 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1212 ms, Memory: 55760 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static java.util.Collections.reverseOrder;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String command = st.nextToken();
+            if(command.equals("enter")){
+                set.add(name);
+            }
+            else{
+                set.remove(name);
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+        for(String name : set){
+            list.add(name);
+        }
+
+        Collections.sort(list, reverseOrder());
+        for(String name : list){
+            System.out.println(name);
+        }
+    }
+}
