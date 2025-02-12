fbb0af0d9adc76c227a74ec3f78b32d5cbf93046
strangehoon <strangehoon2@gmail.com>
2025-02-12 16:44:41 +0900

    Update 쿼드압축 후 개수 세기.java

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,14 +1,12 @@
 class Solution {
     
-    public void calculate(int x1, int y1, int x2, int y2, int[][] arr, int[] answer){
-        
-        int side = x2-x1+1;
+    public void calculate(int x1, int y1, int size, int[][] arr, int[] answer){
         
         // s내부에 있는 모든 수가 같은 값인지 검사
         int std = arr[x1][y1];
         boolean flag = true;
-        for(int i=x1; i<=x2; i++){
-            for(int j=y1; j<=y2; j++){
+        for(int i=x1; i<x1+size; i++){
+            for(int j=y1; j<y1+size; j++){
                 if(arr[i][j]!=std){
                     flag = false;
                 }
@@ -23,16 +21,16 @@ class Solution {
         }
         // 분할
         else{
-            calculate(x1, y1, x1+side/2-1, y1+side/2-1, arr, answer);
-            calculate(x1, y1+side/2, x1+side/2-1, y2, arr, answer);
-            calculate(x1+side/2, y1, x2, y1+side/2-1, arr, answer);
-            calculate(x1+side/2, y1+side/2, x2, y2, arr, answer);
+            calculate(x1, y1, size/2, arr, answer);
+            calculate(x1, y1+size/2, size/2, arr, answer);
+            calculate(x1+size/2, y1, size/2, arr, answer);
+            calculate(x1+size/2, y1+size/2, size/2, arr, answer);
         }
     }
     
     public int[] solution(int[][] arr) {
         int[] answer = new int[2];        
-        calculate(0, 0, arr.length-1, arr[0].length-1, arr, answer);
+        calculate(0, 0, arr.length, arr, answer);
 
 
         return answer;
