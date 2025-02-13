81591997bf471343bcb936e09b1146b4bb1a21a0
strangehoon <strangehoon2@gmail.com>
2025-02-14 02:39:19 +0900

    Update 크레인 인형뽑기 게임.java

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -6,26 +6,21 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         int cnt = 0;
         for(int move : moves){
-            boolean flag=false;
             int doll = 0;
             for(int i=0; i<board.length; i++){
                 doll = board[i][move-1];
                 if(doll!=0){
                     board[i][move-1]=0;
-                    flag=true;
+                    // 바구니에 푸시
+                    if(stack.isEmpty() || stack.peek()!=doll){
+                        stack.push(doll);
+                    }
+                    // 바구니에서 팝
+                    else{
+                        stack.pop();
+                        cnt+=2;
+                    }
                     break;
-                }
-            }
-            // 인형을 집었을 경우
-            if(flag){
-                // 바구니에 푸시
-                if(stack.isEmpty() || stack.peek()!=doll){
-                    stack.push(doll);
-                }
-                // 바구니에서 팝
-                else{
-                    stack.pop();
-                    cnt+=2;
                 }
             }
         }
