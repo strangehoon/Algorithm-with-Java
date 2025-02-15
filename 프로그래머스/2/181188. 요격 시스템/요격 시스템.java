c5525cf638e89941390c596826dd3ff7af7de81b
strangehoon <strangehoon2@gmail.com>
2025-02-16 03:39:45 +0900

    [level 2] Title: 요격 시스템, Time: 367.45 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,34 +1,16 @@
 import java.util.*;
 class Solution {
 
-    public class Target implements Comparable<Target>{
-        int start;
-        int end;
-        
-        public Target(int start, int end){
-            this.start = start;
-            this.end = end;
-        }
-        
-        @Override
-        public int compareTo(Target t){
-            return this.end-t.end;
-        }
-    }
-    
+
     public int solution(int[][] targets) {
-        PriorityQueue<Target> pq = new PriorityQueue<>();
+        
+        Arrays.sort(targets, (o1, o2)-> o1[1]-o2[1]);
+        int std = 0;
+        int cnt = 0;
+        
         for(int i=0, len=targets.length; i<len; i++){
-            pq.offer(new Target(targets[i][0], targets[i][1]));
-        }
-        
-        int cnt = 1;
-        int std = pq.poll().end;
-        
-        while(!pq.isEmpty()){
-            Target t = pq.poll();
-            if(t.start>=std){
-                std = t.end;
+            if(targets[i][0]>=std){
+                std = targets[i][1];
                 cnt++;
             }
         }
