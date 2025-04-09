d636418c182d2e5be3259e64f5236d3a2d6748ee
strangehoon <strangehoon2@gmail.com>
2025-04-09 19:43:13 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,17 +2,15 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
-        int len = clothes.length;
-        for(int i=0; i<len; i++){
-            String type = clothes[i][1];
-            map.put(type, map.getOrDefault(type, 0)+1);
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1],0)+1);
         }
-        Set<String> keySet = map.keySet();
-        int result = 1;
-        for(String str : keySet){
-            result *= (map.get(str)+1);
+        
+        int cnt = 1;
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            cnt *= (entry.getValue()+1);
         }
-        result -= 1;
-        return result;
+        
+        return cnt-1;
     }
 }
