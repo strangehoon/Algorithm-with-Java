ac60a14aa67a2f92786f33a3e942711b54246b55
strangehoon <strangehoon2@gmail.com>
2025-04-09 19:21:15 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 60.58 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,43 +1,34 @@
 import java.util.*;
 class Solution {
+    
     public int[] solution(String[] gems) {
+        
         Set<String> set = new HashSet<>();
-        for(String gem : gems){
-            set.add(gem);
+        for(int i=0; i<gems.length; i++){
+            set.add(gems[i]);
         }
-        int right = 0;
-        int left = 0;
-        int max_value = Integer.MAX_VALUE;
+        int typeCnt = set.size();
+        
+        int minLen = Integer.MAX_VALUE;
+        int[] answers = new int[2];
         Map<String, Integer> map = new HashMap<>();
-        int[] answer = new int[2];
-        while(left<=right){
-            if(map.size()<set.size()){
-                if(right==gems.length)
-                    break;
-                String str = gems[right];
-                map.put(str, map.getOrDefault(str,0)+1);
-                right++;
-            }
-            else{
-                if(right-left<max_value){
-                    max_value = right-left;
-                    answer[0] = left+1;
-                    answer[1] = right;
+        int j=0;
+        for(int i=0; i<gems.length; i++){
+            map.put(gems[i], map.getOrDefault(gems[i], 0)+1);
+            
+            while(map.size()==typeCnt){
+                if(i-j+1<minLen){
+                    minLen = Math.min(minLen, i-j+1);
+                    answers[0]=j+1;
+                    answers[1]=i+1;
                 }
-                String str = gems[left];
-                map.put(str, map.get(str)-1);
-                if(map.get(str)==0){
-                    map.remove(str);
-                }
-                left++;
+                String tem = gems[j++];
+                map.put(tem, map.get(tem)-1);
+                if(map.get(tem)==0)
+                    map.remove(tem);
             }
         }
-        if(map.size()==set.size()){
-            if(answer[0]==0 && answer[1]==0){
-                answer[0] = left+1;
-                answer[1] = gems.length;    
-            }
-        }
-        return answer;
+        
+        return answers;
     }
 }
