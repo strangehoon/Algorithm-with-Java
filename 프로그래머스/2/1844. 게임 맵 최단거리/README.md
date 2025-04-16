d980f8cbb2a0d01a3d5efd03292b23ba46281133
strangehoon <strangehoon2@gmail.com>
2025-04-16 21:42:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.34 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 53.1 MB, 시간: 8.90 ms
+메모리: 54.4 MB, 시간: 8.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 19:33:18
+2025년 04월 16일 21:42:26
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,53 +1,39 @@
 import java.util.*;
-
-class Pos{
-    int x;
-    int y;
-    int d;
-    
-    public Pos(int x, int y, int d){
-        this.x = x;
-        this. y = y;
-        this.d = d;
-    }
-}
-
 class Solution {
-    
-    int[] dx = {0, 0, 1, -1};
-    int[] dy = {1, -1, 0, 0};
-    int[][] maps;
-    boolean[][] visited;
-    
+
     public int solution(int[][] maps) {
-        this.maps = maps;
-        this.visited = new boolean[maps.length][maps[0].length];
         
-        Queue<Pos> queue = new LinkedList<>();
-        queue.offer(new Pos(0, 0, 1));
+        int row = maps.length;
+        int col = maps[0].length;
+        boolean[][] visited = new boolean[row][col];
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 1});
         visited[0][0] = true;
         
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
+        
         while(!queue.isEmpty()){
-            Pos nowPos = queue.poll();
-            int nowX = nowPos.x;
-            int nowY = nowPos.y;
-            int nowD = nowPos.d;
+            int[] pos = queue.poll();
+            int x = pos[0];
+            int y = pos[1];
+            int cnt = pos[2];
             
-            if(nowX==maps.length-1 && nowY==maps[0].length-1){
-                return nowD;
-            }
-            
+            if(x==row-1 && y==col-1)
+                return cnt;
             
             for(int i=0; i<4; i++){
-                int nx = nowX + dx[i];
-                int ny = nowY + dy[i];
-                if(0<=nx && nx<maps.length && 0<=ny && ny<maps[0].length){
-                    if(maps[nx][ny]==1 && visited[nx][ny]==false){
-                        visited[nx][ny] = true;
-                        queue.offer(new Pos(nx, ny, nowD+1));
-                    }    
-                }    
-            }
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx<0 || nx>=row || ny<0 || ny>=col)
+                    continue;
+                
+                if(!visited[nx][ny] && maps[nx][ny]==1){
+                    visited[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny, cnt+1});
+                }
+            }    
         }
         
         return -1;
