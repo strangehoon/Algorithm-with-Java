6209c9e215df1931f47fa7cb5dda460db3f7a0da
strangehoon <strangehoon2@gmail.com>
2025-04-23 19:40:55 +0900

    [level 2] Title: 소수 찾기, Time: 32.33 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,38 @@
 import java.util.*;
 class Solution {
-    public int len;
-    public boolean[] visited;
-    public char[] card;
-    public int result = 0;
-    public HashSet<Integer> set = new HashSet<>();
-    
-    public void DFS(String str, int num){
-        if(num==len){
-            if(!str.equals("") && !set.contains(Integer.valueOf(str))){
-                if(isPrime(Integer.valueOf(str))){
-                    set.add(Integer.valueOf(str));
-                    result++;
-                }
-            }
-            return;
-        }
-        
-        for(int i=0; i<len; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                DFS(str+card[i], num+1);
-                visited[i] = false;
-                DFS(str, num+1);
-            }
-        }
-    }
-    
+
     public boolean isPrime(int num){
-        boolean flag = true;
         if(num==0 || num==1)
             return false;
+        
         for(int i=2; i<=Math.sqrt(num); i++){
-            if(num%i==0){
-                flag = false;
-                return flag;
-            }
+            if(num%i==0)
+                return false;
         }
-        return flag;
+        return true;
     }
     
-    public int solution(String numbers) {    
-        len = numbers.length();
-        card = numbers.toCharArray();
-        visited = new boolean[len];
-        DFS("", 0);
+    public void dfs(char[] charArr, String numStr, boolean[] isUsed, Set<Integer> answerSet){
         
-        return result;
+        int num = Integer.parseInt(numStr);
+        if(isPrime(num))
+            answerSet.add(num);
+        
+        for(int i=0; i<charArr.length; i++){
+            if(!isUsed[i]){
+                isUsed[i] = true;
+                dfs(charArr, numStr+charArr[i], isUsed, answerSet);
+                isUsed[i] = false;
+            }
+        }
+    }
+    
+    public int solution(String numbers) {
+        char[] charArr = numbers.toCharArray();
+        boolean[] isUsed = new boolean[numbers.length()];
+        Set<Integer> answerSet = new HashSet<>();
+        
+        dfs(charArr, "0", isUsed, answerSet);
+        return answerSet.size();
     }
 }
