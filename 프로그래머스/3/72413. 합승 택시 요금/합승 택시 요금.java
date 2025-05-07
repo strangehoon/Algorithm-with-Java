9ccc680e37beb7196ea0536c1340cd53dd2193a9
strangehoon <strangehoon2@gmail.com>
2025-05-07 20:38:00 +0900

    Update 합승 택시 요금.java

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,23 +1,24 @@
 import java.util.*;
 
 class Solution {
-    // 4:40
+
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int[][] graph = new int[n][n];
         final int INF = 1000000000;
         
+        // 그래프 초기화
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(i!=j)
                     graph[i][j] = INF;
             }
         }
-        
         for(int[] fare : fares){
             graph[fare[0]-1][fare[1]-1] = Math.min(graph[fare[0]-1][fare[1]-1], fare[2]);
             graph[fare[1]-1][fare[0]-1] = Math.min(graph[fare[1]-1][fare[0]-1], fare[2]);
         }
         
+        // 플로이드 와셜 알고리즘 수행
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 for(int k=0; k<n; k++){
@@ -27,27 +28,12 @@ class Solution {
         }
         
         int minValue = INF;
-        int minIdx = -1;
         for(int i=0; i<n; i++){
-            int tem = 0;
-            boolean flag = true;
-            for(int j=0; j<n; j++){
-                if(j==s-1 || j==a-1 || j==b-1){
-                    if(graph[i][j]==INF){
-                        flag = false;
-                        break;
-                    }
-                    tem += graph[i][j];
-                }
-            }
-            if(flag && tem<minValue){
-                minIdx = i;
-                minValue = tem;
-            }
+            if(graph[i][s-1]==INF || graph[i][a-1]==INF || graph[i][b-1]==INF)
+                continue;
+            minValue = Math.min(minValue, graph[i][s-1] + graph[i][a-1] + graph[i][b-1]);
         }
     
-        int answer = graph[minIdx][s-1] + graph[minIdx][a-1] + graph[minIdx][b-1];
-        
-        return answer;
+        return minValue;
     }
 }
