1e7bf0c54cdf9eccf48c4b4f422935bc46c438a7
strangehoon <strangehoon2@gmail.com>
2025-05-08 20:21:40 +0900

    [Gold III] Title: 파티, Time: 184 ms, Memory: 18764 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -5,18 +5,43 @@ import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-    public static class Edge implements Comparable<Edge>{
-        int vertex;
-        int cost;
 
-        public Edge(int vertex, int cost){
+    public static class Edge implements Comparable<Edge> {
+        int vertex;
+        int value;
+
+        public Edge(int vertex, int value) {
             this.vertex = vertex;
-            this.cost = cost;
+            this.value = value;
         }
 
         @Override
-        public int compareTo(Edge e){
-            return e.cost - this.cost;
+        public int compareTo(Edge e) {
+            return this.value - e.value;
+        }
+    }
+    public static int N;
+    public static int[] answers;
+
+    public static void dijk(int X, List<List<Edge>> graph, int[] dist){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(X, 0));
+
+        while (!pq.isEmpty()){
+            Edge now = pq.poll();
+            int vertex = now.vertex;
+            int value = now.value;
+
+            for(Edge edge : graph.get(vertex)){
+                if(value+edge.value<dist[edge.vertex]){
+                    dist[edge.vertex] = value+ edge.value;
+                    pq.offer(new Edge(edge.vertex, dist[edge.vertex]));
+                }
+            }
+        }
+
+        for(int i=1; i<=N; i++){
+            answers[i] += dist[i];
         }
     }
 
@@ -25,9 +50,9 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.valueOf(st.nextToken());
-        int M = Integer.valueOf(st.nextToken());
-        int X = Integer.valueOf(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
         List<List<Edge>> graph = new ArrayList<>();
         List<List<Edge>> reverseGraph = new ArrayList<>();
@@ -36,61 +61,32 @@ public class Main {
             reverseGraph.add(new ArrayList<>());
         }
 
-        for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.valueOf(st.nextToken());
-            int b = Integer.valueOf(st.nextToken());
-            int t = Integer.valueOf(st.nextToken());
-            graph.get(a).add(new Edge(b, t));
-            reverseGraph.get(b).add(new Edge(a, t));
-        }
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        PriorityQueue<Edge> reversePq = new PriorityQueue<>();
-        pq.offer(new Edge(X, 0));
-        reversePq.offer(new Edge(X, 0));
         int[] dist = new int[N+1];
         int[] reverseDist = new int[N+1];
-        for(int i=1; i<=N; i++){
-            dist[i] = Integer.MAX_VALUE;
-            reverseDist[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(reverseDist, Integer.MAX_VALUE);
+
         dist[X] = 0;
         reverseDist[X] = 0;
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph.get(u).add(new Edge(v, w));
+            reverseGraph.get(v).add(new Edge(u, w));
+        }
 
-        while(!pq.isEmpty()){
-            Edge edge = pq.poll();
-            int now = edge.vertex;
-            int nowCost = edge.cost;
-            if(dist[now]<nowCost)
-                continue;
-            for(Edge tem : graph.get(now)){
-                if(dist[tem.vertex]>dist[now]+tem.cost){
-                    dist[tem.vertex] = dist[now] + tem.cost;
-                    pq.offer(new Edge(tem.vertex, dist[tem.vertex]));
-                }
-            }
-        }
-        while(!reversePq.isEmpty()){
-            Edge edge = reversePq.poll();
-            int now = edge.vertex;
-            int nowCost = edge.cost;
-            if(reverseDist[now]<nowCost)
-                continue;
-            for(Edge tem : reverseGraph.get(now)){
-                if(reverseDist[tem.vertex]>reverseDist[now]+tem.cost){
-                    reverseDist[tem.vertex] = reverseDist[now] + tem.cost;
-                    reversePq.offer(new Edge(tem.vertex, reverseDist[tem.vertex]));
-                }
-            }
-        }
-        int result = Integer.MIN_VALUE;
+        answers = new int[N+1];
+        dijk(X, graph, dist);
+        dijk(X, reverseGraph, reverseDist);
+
+        int maxValue = Integer.MIN_VALUE;
         for(int i=1; i<=N; i++){
-            int tem = dist[i] + reverseDist[i];
-            result = Math.max(result, tem);
+            maxValue = Math.max(maxValue, answers[i]);
         }
 
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(maxValue));
         bw.flush();
         bw.close();
         br.close();
