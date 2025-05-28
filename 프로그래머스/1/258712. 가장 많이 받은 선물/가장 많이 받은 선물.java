c996b2fa42f464003f95f96f905c4a2e2f59e56f
strangehoon <strangehoon2@gmail.com>
2025-05-29 02:22:51 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 30.73 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,53 +1,56 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        HashMap<String, Integer> friendsMap = new HashMap<>();
+        Map<String, Map<String, Integer>> history = new HashMap<>();
+        Map<String, Integer> score = new HashMap<>();
+        
+        // 초기화
         for(int i=0; i<friends.length; i++){
-            friendsMap.put(friends[i], i);
+            history.put(friends[i], new HashMap<>());
         }
         
-        int[][] giftGraph = new int[friends.length][friends.length];
+        // 선물 기록 분석
         for(String gift : gifts){
-            String[] tem = gift.split(" ");
-            String sender = tem[0];
-            String receiver = tem[1];
-            int senderIdx = friendsMap.get(sender);
-            int receiverIdx = friendsMap.get(receiver);
-            giftGraph[senderIdx][receiverIdx] += 1;
+            String sender = gift.split(" ")[0];
+            String receiver = gift.split(" ")[1];
+            
+            history.get(sender).put(receiver, history.get(sender).getOrDefault(receiver, 0)+1);
+            score.put(sender, score.getOrDefault(sender, 0)+1);
+            score.put(receiver, score.getOrDefault(receiver, 0)-1);
         }
         
-        int[] giftScore = new int[friends.length];
+        // 친구별로 다음달에 받는 선물 수 계산
+        HashMap<String, Integer> cntMap = new HashMap<>();
         for(int i=0; i<friends.length; i++){
-            int score =  0;
-            for(int j=0; j<friends.length; j++){
-                score += giftGraph[i][j];
+            for(int j=i+1; j<friends.length; j++){
+                String a = friends[i];
+                String b = friends[j];
+                int aToB = history.get(a).getOrDefault(b, 0);
+                int bToA = history.get(b).getOrDefault(a, 0);
+                if(aToB>bToA){
+                    cntMap.put(a, cntMap.getOrDefault(a, 0)+1);
+                }
+                else if(aToB<bToA){
+                    cntMap.put(b, cntMap.getOrDefault(b, 0)+1);
+                }
+                else{
+                    int scoreA = score.getOrDefault(a, 0);
+                    int scoreB = score.getOrDefault(b, 0);
+                    if(scoreA>scoreB){
+                        cntMap.put(a, cntMap.getOrDefault(a, 0)+1);
+                    }
+                    else if(scoreA<scoreB){
+                        cntMap.put(b, cntMap.getOrDefault(b, 0)+1);
+                    }
+                }
             }
-            int b = 0;
-            for(int j=0; j<friends.length; j++){
-                score -= giftGraph[j][i];
-            }
-            giftScore[i] = score;
-        }
-        
-        int result[] = new int[friends.length];
-        for(int i=0; i<friends.length; i++){
-            for(int j=0; j<friends.length; j++){
-                if(i==j)
-                    continue;
-                int a = giftGraph[i][j];
-                int b = giftGraph[j][i];
-                if(a>b)
-                    result[i]++;
-                else if(a==b && giftScore[i] > giftScore[j])
-                    result[i]++;
-            }
-        }
-        int max = 0;
-        for(int x : result){
-            max = Math.max(x, max);
         }
     
-        
-        return max;
+        // 가장 많은 선물을 받은 사람의 선물의 수 구하기
+        int result = 0;
+        for(int cnt : cntMap.values())
+            result = Math.max(result, cnt);
+        return result;
     }
 }
