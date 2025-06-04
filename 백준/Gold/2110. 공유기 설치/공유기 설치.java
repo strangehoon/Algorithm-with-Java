5e56fa5aa0a5563b41fa96391ce65d18e2021d30
strangehoon <strangehoon2@gmail.com>
2025-06-04 22:17:30 +0900

    [Gold IV] Title: 공유기 설치, Time: 344 ms, Memory: 34420 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,48 +1,44 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.valueOf(st.nextToken());
-        int C = Integer.valueOf(st.nextToken());
-        int[] arr = new int[N];
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<N; i++){
-            arr[i] = Integer.valueOf(br.readLine());
+            int x = Integer.parseInt(br.readLine());
+            list.add(x);
         }
-        Arrays.sort(arr);
-        int result = Integer.MIN_VALUE;
-        int start = 1;
-        int end = arr[N-1];
-        while(start<=end){
-            int mid = (start+end)/2;
+        Collections.sort(list);
+
+        int idx = 1;
+        int jdx = 1000000000;
+        int answer = 0;
+        while(idx<=jdx){
+            int mid = (idx+jdx)/2;
+            int preIdx = 0;
             int cnt = 1;
-            int tem = arr[0];
             for(int i=1; i<N; i++){
-                if(arr[i]>=mid+tem){
-                    cnt++;
-                    tem = arr[i];
+                if(list.get(i)-list.get(preIdx)>=mid){
+                    cnt ++;
+                    preIdx = i;
                 }
             }
-            if(cnt<C){
-                end = mid-1;
+            if(cnt>=C){
+                idx = mid+1;
+                answer = Math.max(answer, mid);
             }
             else{
-                start = mid+1;
-                result = Math.max(result, mid);
+                jdx = mid-1;
             }
         }
-
-        bw.write(String.valueOf(result));
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(answer);
     }
 }
