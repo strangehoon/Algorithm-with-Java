1c7a13b0ec8d4cafd201f0626523cabcf4cc5da2
strangehoon <strangehoon2@gmail.com>
2025-06-12 11:20:14 +0900

    Update 연속 펄스 부분 수열의 합.java

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,29 +1,25 @@
 class Solution {
+    // 점화식 : dp[i] = Math.max(dp[i-1] + sequence[i], sequence[i]);
     public long solution(int[] sequence) {
         
         int len = sequence.length;
-        long[] purse1 = new long[len];
-        long[] purse2 = new long[len];
-        
-        for(int i=0; i<len; i++){
-            purse1[i] = sequence[i]*((i%2==0) ? 1 : -1);
-            purse2[i] = sequence[i]*((i%2==1) ? 1 : -1);
-        }
-        
+
         long[] dp1 = new long[len];
         long[] dp2 = new long[len];
         
-        dp1[0] = purse1[0];
-        dp2[0] = purse2[0];
+        dp1[0] = sequence[0];
+        dp2[0] = -sequence[0];
         
         long result = Math.max(dp1[0], dp2[0]);
         for(int i=1; i<len; i++){
-            dp1[i] = Math.max(dp1[i-1] + purse1[i], purse1[i]);
-            dp2[i] = Math.max(dp2[i-1] + purse2[i], purse2[i]);
-            result = Math.max(result, dp1[i]);
-            result = Math.max(result, dp2[i]);
+            long pulse1 = sequence[i]*((i%2==0) ? 1 : -1);
+            long pulse2 = sequence[i]*((i%2==1) ? 1 : -1);
+            
+            dp1[i] = Math.max(dp1[i-1] + pulse1, pulse1);
+            dp2[i] = Math.max(dp2[i-1] + pulse2, pulse2);
+            
+            result = Math.max(result, Math.max(dp1[i], dp2[i]));
         }
-        
         return result;
     }
 }
