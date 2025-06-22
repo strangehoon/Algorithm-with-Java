3e36ca07eb1acf6037e8e0fdd6092dc09355daa3
strangehoon <strangehoon2@gmail.com>
2025-06-22 19:00:32 +0900

    [level 1] Title: 여러 기준으로 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
+++ 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
@@ -1,2 +1,6 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, DATETIME FROM ANIMAL_INS ORDER BY NAME ASC, DATETIME DESC;
+SELECT animal_id,
+       name,
+       datetime
+FROM animal_ins
+ORDER BY name, datetime desc
