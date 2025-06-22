d8d05579882a21f9122ed9fdae708c48d2b7a73c
strangehoon <strangehoon2@gmail.com>
2025-06-22 18:50:04 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,2 +1,5 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME FROM ANIMAL_INS WHERE INTAKE_CONDITION = "Sick" ORDER BY ANIMAL_ID;
+SELECT animal_id, name
+FROM animal_ins
+WHERE intake_condition = 'Sick'
+ORDER BY animal_id
