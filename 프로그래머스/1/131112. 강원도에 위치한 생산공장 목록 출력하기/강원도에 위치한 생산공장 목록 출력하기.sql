783c60e46ca1cbb28431db0c708ce051cbbba9fa
strangehoon <strangehoon2@gmail.com>
2025-06-22 16:37:48 +0900

    [level 1] Title: 강원도에 위치한 생산공장 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
+++ 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
@@ -1,2 +1,5 @@
 -- 코드를 입력하세요
-SELECT FACTORY_ID, FACTORY_NAME, ADDRESS FROM FOOD_FACTORY WHERE ADDRESS LIKE "강원도%" ORDER BY FACTORY_ID;
+SELECT factory_id, factory_name, address
+FROM food_factory
+WHERE address like '강원도%'
+ORDER BY factory_id
