cb8e8d834d0c14351188d993ded54eecc06b5429
strangehoon <strangehoon2@gmail.com>
2025-07-04 18:44:17 +0900

    Update 택배 배달과 수거하기.java

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,48 +2,32 @@ import java.util.*;
 
 class Solution {
     
-    class Delivery implements Comparable<Delivery>{
+    class Task implements Comparable<Task>{
         int distance;
         int cnt;
         
-        public Delivery(int distance, int cnt){
+        public Task(int distance, int cnt){
             this.distance = distance;
             this.cnt = cnt;
         }
         
         @Override
-        public int compareTo(Delivery delivery){
-            return delivery.distance-this.distance;
-        }
-    }
-    
-    class Pickup implements Comparable<Pickup>{
-        int distance;
-        int cnt;
-        
-        public Pickup(int distance, int cnt){
-            this.distance = distance;
-            this.cnt = cnt;
-        }
-        
-        @Override
-        public int compareTo(Pickup pickup){
-            return pickup.distance-this.distance;
+        public int compareTo(Task task){
+            return task.distance-this.distance;
         }
     }
     
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         
-        PriorityQueue<Delivery> deliveryPq = new PriorityQueue<>();
-        PriorityQueue<Pickup> pickupPq = new PriorityQueue<>();
+        PriorityQueue<Task> deliveryPq = new PriorityQueue<>();
+        PriorityQueue<Task> pickupPq = new PriorityQueue<>();
         
-        int len = deliveries.length;
-        for(int i=0; i<len; i++){
+        for(int i=0; i<n; i++){
             if(deliveries[i]!=0){
-                deliveryPq.offer(new Delivery(i+1, deliveries[i]));
+                deliveryPq.offer(new Task(i+1, deliveries[i]));
             }
             if(pickups[i]!=0){
-                pickupPq.offer(new Pickup(i+1, pickups[i]));
+                pickupPq.offer(new Task(i+1, pickups[i]));
             }
         }
         
@@ -52,7 +36,6 @@ class Solution {
             
             int remainDelivery = cap;
             int remainPickup = cap;
-            
             int maxDistance = 0;
             
             if(!deliveryPq.isEmpty()){
@@ -66,27 +49,27 @@ class Solution {
             answer += maxDistance*2;
             
             while(remainDelivery>0 && !deliveryPq.isEmpty()){
-                Delivery curDelivery = deliveryPq.poll();
-                remainDelivery -= curDelivery.cnt;
+                Task curTask = deliveryPq.poll();
+                remainDelivery -= curTask.cnt;
                 
                 if(remainDelivery==0)
                     break;
                 
                 else if(remainDelivery<0){
-                    deliveryPq.offer(new Delivery(curDelivery.distance, remainDelivery*-1));
+                    deliveryPq.offer(new Task(curTask.distance, remainDelivery*-1));
                     break;
                 }
             }
             
             while(remainPickup>0 && !pickupPq.isEmpty()){
-                Pickup curPickup = pickupPq.poll();
-                remainPickup -= curPickup.cnt;
+                Task curTask = pickupPq.poll();
+                remainPickup -= curTask.cnt;
                 
                 if(remainPickup==0)
                     break;
                 
                 if(remainPickup<0){
-                    pickupPq.offer(new Pickup(curPickup.distance, remainPickup*-1));
+                    pickupPq.offer(new Task(curTask.distance, remainPickup*-1));
                     break;
                 }
             }
