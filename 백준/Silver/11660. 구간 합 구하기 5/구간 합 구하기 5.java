741f692f77cc3f7fa45b5aa3849a0646990fb1d9
strangehoon <strangehoon2@gmail.com>
2025-07-16 02:37:28 +0900

    Update 구간 합 구하기 5.java

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,46 +1,37 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.valueOf(st.nextToken());
-        int M = Integer.valueOf(st.nextToken());
-        int[][] graph = new int[N+1][N+1];
-        int[][] sumArr = new int[N+1][N+1];
-        for(int i=1; i<=N; i++){
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] graph = new int[n+1][n+1];
+        for(int i=1; i<=n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=N; j++){
-                int num = Integer.valueOf(st.nextToken());
-                graph[i][j] = num;
+            for(int j=1; j<=n; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j] - sumArr[i-1][j-1] + graph[i][j];
+        int[][] dp = new int[n+1][n+1];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + graph[i][j];
             }
         }
 
-        for(int i=0; i<M; i++){
+        for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.valueOf(st.nextToken());
-            int y1 = Integer.valueOf(st.nextToken());
-            int x2 = Integer.valueOf(st.nextToken());
-            int y2 = Integer.valueOf(st.nextToken());
-            int result = sumArr[x2][y2]-sumArr[x2][y1-1]-sumArr[x1-1][y2]+sumArr[x1-1][y1-1];
-            bw.write(String.valueOf(result)+"\n");
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            System.out.println(dp[x2][y2]-dp[x2][y1-1]-dp[x1-1][y2]+dp[x1-1][y1-1]);
         }
-
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
