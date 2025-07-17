3e34e6f96a687e39e44fad21092821e4d6162922
strangehoon <strangehoon2@gmail.com>
2025-07-17 12:17:43 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 33.53 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,5 +1,4 @@
 class Solution {
-    // 점화식 : dp[i] = Math.max(dp[i-1] + sequence[i], sequence[i]);
     public long solution(int[] sequence) {
         
         int len = sequence.length;
@@ -10,7 +9,8 @@ class Solution {
         dp1[0] = sequence[0];
         dp2[0] = -sequence[0];
         
-        long result = Math.max(dp1[0], dp2[0]);
+        long answer = Math.max(dp1[0], dp2[0]);
+        
         for(int i=1; i<len; i++){
             long pulse1 = sequence[i]*((i%2==0) ? 1 : -1);
             long pulse2 = sequence[i]*((i%2==1) ? 1 : -1);
@@ -18,8 +18,8 @@ class Solution {
             dp1[i] = Math.max(dp1[i-1] + pulse1, pulse1);
             dp2[i] = Math.max(dp2[i-1] + pulse2, pulse2);
             
-            result = Math.max(result, Math.max(dp1[i], dp2[i]));
+            answer = Math.max(answer, Math.max(dp1[i], dp2[i]));
         }
-        return result;
+        return answer;
     }
 }
