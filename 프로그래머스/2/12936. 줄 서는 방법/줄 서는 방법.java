c0c30d26116b3804a117bc4cf6f1dcef92806071
strangehoon <strangehoon2@gmail.com>
2025-08-21 20:21:22 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.03 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -5,27 +5,27 @@ class Solution {
     public int[] solution(int n, long k) {
         
         int a = n;
-        long b = k;
         int idx = 0;
         int[] answer = new int[n];
         boolean[] used = new boolean[n+1];
         
         while(a-->0){
-
+            
             long tem = 1;
             for(int i=1; i<=a; i++){
                 tem *= i;
             }
                 
-            long num = b/tem;
-            long na = b%tem;
+            long num = k/tem;
+            k = k%tem;
             
-            if(na==0){
-                num = num-1;
+            if(k==0){
+                num = num==-1 ? n-1-idx : num--;
+                num--;
                 if(num==-1)
                     num = n-1-idx;
             }
-            
+  
             for(int i=1; i<=n; i++){
                 if(!used[i]){
                     if(num==0){
@@ -38,75 +38,7 @@ class Solution {
                     }
                 }
             }
-            b = na;
         }
         return answer;
     }
 }
-
-// 4 16
-// 3 2 4
-// 2 2 0
-// 1 0 0
-    
-// 3 2 4 1  
-
-// 4  17
-// 3  2, 5
-// 2  2, 1    
-// 1  1, 0
-
-// 3 4 1 2
-
-// 4 18
-// 3 3 0
-// 2 0 0
-
-// 3 4 2 1 
-    
-// 4 19 
-// 3 3 1
-// 2 0 1
-// 1 1 0 
-    
-// 4 1 2 3
-    
-// 4 20
-// 3 3 2
-// 2 1 0
-
-// 4 
-    
-
-// 1 2 3 4 
-// 1 2 4 3
-// 1 3 2 4
-// 1 3 4 2
-// 1 4 2 3
-// 1 4 3 2
-// 2 1 3 4
-// 2 1 4 3
-// 2 3 1 4
-// 2 3 4 1
-// 2 4 1 3
-// 2 4 3 1
-// 3 1 2 4
-// 3 1 4 2
-// 3 2 1 4
-// 3 2 4 1
-// 3 4 1 2
-// 3 4 2 1
-// 4 1 2 3
-// 4 1 3 2
-// 4 2 1 3
-// 4 2 3 1
-// 4 3 1 2
-// 4 3 2 1
-
-    
-    
-// 2 2 1
-// 1 1 0 
-    
-    
-// 3 
