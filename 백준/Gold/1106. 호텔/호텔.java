6842851a2378a4e9add8f391a53cd8f43be03d37
strangehoon <strangehoon2@gmail.com>
2025-08-22 19:59:31 +0900

    [Gold IV] Title: 호텔, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -1,44 +1,40 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            
         StringTokenizer st = new StringTokenizer(br.readLine());
         int C = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        final int maxValue = 100000;
-        
-        int[][] dp = new int[N+1][maxValue+1];
-        int[][] infos = new int[N+1][2];
 
-        for(int i=1; i<=N; i++){
+        int[] cost = new int[N];
+        int[] gain = new int[N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            infos[i][0] = Integer.parseInt(st.nextToken());
-            infos[i][1] = Integer.parseInt(st.nextToken());
+            cost[i] = Integer.parseInt(st.nextToken());
+            gain[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=maxValue; j++){
-                int[] info = infos[i];
+        final int EXTRA = 100;               // gain의 최댓값이 100
+        int LIMIT = C + EXTRA;               // C를 넘어도 달성 가능
+        int INF = 1_000_000_000;
 
-                dp[i][j] = dp[i-1][j];
-                if(j-info[0]>=0){
-                    dp[i][j] = Math.max(dp[i][j-info[0]] + info[1], dp[i][j]);
-                    dp[i][j] = Math.max(dp[i-1][j-info[0]] + info[1], dp[i][j]);
+        int[] dp = new int[LIMIT + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        // 무제한 배낭: 아이템(도시) 밖 루프, 안에서 x를 증가 방향으로
+        for (int i = 0; i < N; i++) {
+            for (int x = gain[i]; x <= LIMIT; x++) {
+                if (dp[x - gain[i]] != INF) {
+                    dp[x] = Math.min(dp[x], dp[x - gain[i]] + cost[i]);
                 }
             }
         }
 
-        for(int i=1; i<=100000; i++){
-            if(dp[N][i]>=C){
-                System.out.println(i);
-                return;
-            }
-        }
+        int ans = INF;
+        for (int x = C; x <= LIMIT; x++) ans = Math.min(ans, dp[x]);
+        System.out.println(ans);
     }
 }
