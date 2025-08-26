1a563ae910b5a0958545bbdf3ac0b657641f7174
strangehoon <strangehoon2@gmail.com>
2025-08-27 03:07:43 +0900

    [level 3] Title: 양과 늑대, Time: 9.77 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,62 +1,60 @@
 import java.util.*;
 
 class Solution {
+
+    public List<List<Integer>> graph;
+    public int[] info;
+    public int answer;
     
-    static List<Integer> parent;
-    static boolean []check;
-    static List<List<Integer>> graph;
-    static int len;
-    static int sheep, wolf, answer;
-    
-    public int solution(int[] info, int[][] edges) {
-        answer = 1;
-        len=info.length;
-        graph =new ArrayList<>();
-
-        for(int i=0; i<len; i++){
-            graph.add(new ArrayList<>());
+    public void dfs(int sheep, int wolf, Set<Integer> remainSet){
+        
+        answer = Math.max(answer, sheep);
+        
+        Set<Integer> temSet = new HashSet<>();
+        for(int num : remainSet){
+            temSet.add(num);
         }
-        for(int i=0; i<len-1; i++){
-            graph.get(edges[i][0]).add(edges[i][1]);
-        }
-        check=new boolean[len];
-        parent= new ArrayList<>();
-        for(int i=0; i<graph.get(0).size(); i++){
-            parent.add(graph.get(0).get(i));
-        }
-        sheep=1;
-        wolf=0;
-        check[0]=true;
-        dfs(0, parent, info);
+        
+        for(int next : remainSet){
+            if(info[next]==0){
+                temSet.remove(next);
+                temSet.addAll(graph.get(next));
+                
+                dfs(sheep+1, wolf, temSet);
+                
+                temSet.add(next);
+                temSet.removeAll(graph.get(next));
+            }
+            else{
 
+                if(sheep>wolf+1){
+                    temSet.remove(next);
+                    temSet.addAll(graph.get(next));
 
-        return answer;
-    }
-    public void dfs(int node, List<Integer> parent, int[] info) {
-        List<Integer> tmp = new ArrayList<>(parent);
-        for (int i = 0; i < parent.size(); i++) {
-            int pick = parent.get(i);
-            if (!check[pick] && sheep > wolf + info[pick]) {
-                if (info[pick] == 0) {
-                    sheep++;
-                } else {
-                    wolf++;
-                }
-                // 자식노드 저장
-                for (int k = 0; k < graph.get(pick).size(); k++) {
-                    tmp.add(graph.get(pick).get(k));
-                }
-                check[pick] = true;
-                answer = Math.max(sheep, answer);
-                dfs(pick, tmp, info);
-                tmp = new ArrayList<>(parent);
-                check[pick] = false;
-                if (info[pick] == 0) {
-                    sheep--;
-                } else {
-                    wolf--;
+                    dfs(sheep, wolf+1, temSet);
+
+                    temSet.add(next);
+                    temSet.removeAll(graph.get(next));
                 }
             }
         }
+    }
+    
+    public int solution(int[] info, int[][] edges) {
+        
+        this.info = info;
+        answer = 0;
+        graph = new ArrayList<>();
+        for(int i=0; i<info.length; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] edge : edges){
+            graph.get(edge[0]).add(edge[1]);
+        }
+        
+        dfs(1, 0, new HashSet<>(graph.get(0)));
+        
+        return answer;
     }
 }
