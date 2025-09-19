6480e549b50a0ff2944a3fe362b72eb6eec5c50a
strangehoon <strangehoon2@gmail.com>
2025-09-20 01:13:22 +0900

    Update 최고의 집합.java

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,5 +1,5 @@
-// 20:20
 import java.util.*;
+
 class Solution {
     public int[] solution(int n, int s) {
         
@@ -18,22 +18,3 @@ class Solution {
         return answer;
     }
 }
-
-
-// 모든 원소의 합은 1억 이하의 자연수이다.
-// 1) 완전 탐색 X
-// 2) 
-    
-// n=2
-// dp: 1 1 2
-// S : 1 2 3 4 5 6 7 8 9 10 11 12 13 14
-    
-// dp[i][j] =
-    
-// for(int i=0; i<n; i++){
-//     for(int j=0; j<s; j++){
-//         for(int k=1; k<10; k++){
-//             dp[i][j] = Math.max(dp[i-1][k-1]*k, dp[i][j]);
-//         }
-//     }
-//
