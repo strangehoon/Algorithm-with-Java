20f5ce627e448242bde785548dacf4e85920323a
strangehoon <strangehoon2@gmail.com>
2025-09-20 01:30:02 +0900

    [level 3] Title: 가장 먼 노드, Time: 40.40 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,46 +1,45 @@
 import java.util.*;
-class Solution {
-    
 
-    public int solution(int n, int[][] edge) {
-        // graph init
+class Solution {
+    public int solution(int n, int[][] edges) {
+        
         List<List<Integer>> graph = new ArrayList<>();
         for(int i=0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
-        for(int[] x : edge){
-            graph.get(x[0]).add(x[1]);
-            graph.get(x[1]).add(x[0]);
+        
+        for(int[] edge : edges){
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
         
-        // dist init
-        int[] dist = new int[n+1];
-        for(int i=1; i<=n; i++){
-            dist[i] = -1;
-        }
+        int[] distance = new int[n+1];
+        Arrays.fill(distance, -1);
+        distance[1] = 0;
         
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(1);
-        dist[1] = 0;
+        
         while(!queue.isEmpty()){
-            int tem = queue.poll();
-            for(int num : graph.get(tem)){
-                if(dist[num]==-1){
-                    dist[num] = dist[tem]+1;
-                    queue.offer(num);
+            int cur = queue.poll();
+            
+            for(int next : graph.get(cur)){
+                if(distance[next]==-1){
+                    distance[next] = distance[cur]+1;
+                    queue.offer(next);
                 }
             }
         }
         
-        // result
-        int max_value = Integer.MIN_VALUE;
+        int maxDist = 0;
         int cnt = 0;
         for(int i=1; i<=n; i++){
-            max_value = Math.max(max_value, dist[i]);
-        }
-        for(int i=1; i<=n; i++){
-            if(dist[i]==max_value){
-                cnt++;
+            if(distance[i]>maxDist){
+                maxDist = distance[i];
+                cnt = 1;
+            }
+            else if(distance[i]==maxDist){
+                cnt ++;
             }
         }
         return cnt;
