212cec52a5627eba44b0af38d6e32e895a11efbf
strangehoon <strangehoon2@gmail.com>
2025-09-20 01:08:05 +0900

    [level 3] Title: 최고의 집합, Time: 0.27 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,25 +1,39 @@
+// 20:20
 import java.util.*;
-
 class Solution {
     public int[] solution(int n, int s) {
-        int[] answer = new int[n];
-        int mok = s/n;
-        int rest = s%n;
         
-        if(mok==0){
-            answer = new int[1];
-            answer[0] = -1;
+        if(n>s)
+            return new int[]{-1};
+        
+        int a = s/n;
+        int b = s%n;
+        
+        int[] answer = new int[n];
+        Arrays.fill(answer, a);
+        
+        for(int i=0; i<b; i++){
+            answer[n-1-i]++;
         }
-        else{
-            for(int i=0; i<n; i++){
-                answer[i] = mok;
-                if(rest>0){
-                    answer[i] += 1;
-                    rest--;
-                }
-            }
-        }
-        Arrays.sort(answer);
         return answer;
     }
 }
+
+
+// 모든 원소의 합은 1억 이하의 자연수이다.
+// 1) 완전 탐색 X
+// 2) 
+    
+// n=2
+// dp: 1 1 2
+// S : 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+    
+// dp[i][j] =
+    
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<s; j++){
+//         for(int k=1; k<10; k++){
+//             dp[i][j] = Math.max(dp[i-1][k-1]*k, dp[i][j]);
+//         }
+//     }
+//
