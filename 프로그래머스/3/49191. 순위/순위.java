9345f2418475865010a5535a728f7665b1937346
strangehoon <strangehoon2@gmail.com>
2025-10-03 23:58:55 +0900

    [level 3] Title: 순위, Time: 15.10 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,43 +1,58 @@
 import java.util.*;
+
 class Solution {
+    
+    public void bfs(int num, boolean[] visited, List<List<Integer>> graph){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(num);
+        visited[num] = true;
+        
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            
+            for(int next : graph.get(cur)){
+                if(!visited[next]){
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+    
     public int solution(int n, int[][] results) {
         
-        int[][] graph = new int[n][n];
-        int answer = 0; 
-        for(int i=0; i<n; i++){
-            graph[i][i] = Integer.MAX_VALUE;
+        List<List<Integer>> graph = new ArrayList<>();
+        List<List<Integer>> reversedGraph = new ArrayList<>();
+        
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
+            reversedGraph.add(new ArrayList<>());
         }
-
-        for(int i=0; i<results.length; i++){
-            int a = results[i][0]-1;
-            int b = results[i][1]-1;
-            graph[a][b] = 1;
-            graph[b][a] = -1;
+        
+        for(int[] result : results){
+            graph.get(result[0]).add(result[1]);
+            reversedGraph.get(result[1]).add(result[0]);   
         }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                for(int k=0; k<n; k++){
-                    if(graph[j][i]==1 && graph[i][k]==1){
-                        graph[j][k] = 1;
-                    }
-                    else if(graph[j][i]==-1 && graph[i][k]==-1){
-                        graph[j][k] = -1;
-                    }
-                }
-            }
-        }
-
-        for(int i=0; i<n; i++){
+        
+        int[] cnts = new int[n+1];
+        
+        for(int i=1; i<=n; i++){
+            boolean[] visited = new boolean[n+1];
+            bfs(i, visited, graph);
+            bfs(i, visited, reversedGraph);
+            
             int cnt = 0;
-            for(int j=0; j<n; j++){
-                if(graph[i][j]==1 || graph[i][j]==-1){
+            for(int j=1; j<=n; j++){
+                if(visited[j])
                     cnt++;
-                }
             }
-            if(cnt==n-1){
-                answer ++;
-            }
+            cnts[i] = cnt;
+        }
+        
+        int answer = 0;
+        for(int tem: cnts){
+            if(tem==n)
+                answer++;
         }
         
         return answer;
