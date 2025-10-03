bfe08c33ef07e751b1e9f90d76d9c21b5f52eda8
strangehoon <strangehoon2@gmail.com>
2025-10-03 23:59:18 +0900

    [level 3] Title: 야근 지수, Time: 117.89 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,24 +1,31 @@
 import java.util.*;
+
 class Solution {
     public long solution(int n, int[] works) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int work : works){
-            pq.offer(work);
-        }
-        for(int i=0; i<n; i++){
-            int tem = pq.poll();
-            if(tem==0){
-                return 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<>(){
+            @Override
+            public int compare(Integer i1, Integer i2){
+                return i2-i1;
             }
-            else{
-                pq.offer(tem-1);
-            }
-        }
-        long result = 0;
-        for(int tem : pq){
-            result += tem*tem;
+        });
+        
+        for(int i=0; i<works.length; i++){
+            pq.offer(works[i]);
         }
         
-        return result;
+        while(n-->0 && !pq.isEmpty()){
+            int tem = pq.poll()-1;
+            if(tem>0)
+                pq.offer(tem);
+        }
+        
+        long answer = 0;
+        while(!pq.isEmpty()){
+            int num =pq.poll();
+            answer += num*num;
+        }
+    
+        return answer;
     }
 }
