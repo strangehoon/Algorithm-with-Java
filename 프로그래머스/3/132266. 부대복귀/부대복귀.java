586eea534cc1601bd645a377eb01c87f77c3f680
strangehoon <strangehoon2@gmail.com>
2025-10-03 23:59:28 +0900

    [level 3] Title: 부대복귀, Time: 180.92 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,49 +1,41 @@
-// 8:40- 9:05 , 2:10
-// 왜 dfs로 풀었는데 시간초과 판정이 뜰까...? dp랑 연관이 있을까??? 
-// 1)한번 dp + dfs 문제 풀어보자
-// 2)BFS로 풀어보자
 import java.util.*;
 class Solution {
     
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        List<List<Integer>>graph = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i=0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
         
         for(int i=0; i<roads.length; i++){
-            graph.get(roads[i][0]).add(roads[i][1]);
-            graph.get(roads[i][1]).add(roads[i][0]);
+            int[] road = roads[i];
+            graph.get(road[0]).add(road[1]);
+            graph.get(road[1]).add(road[0]);
         }
         
-        int[] result = new int[sources.length];
         int[] dist = new int[n+1];
-        for(int j=1; j<=n; j++){
-            dist[j] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dist, -1);
         
         Queue<Integer> queue = new LinkedList<>();
+        
+        dist[destination] =0;
         queue.offer(destination);
-        dist[destination] = 0;
         
         while(!queue.isEmpty()){
-            int now = queue.poll();
+            int cur = queue.poll();
             
-            for(int tem : graph.get(now)){
-                if(dist[tem]==Integer.MAX_VALUE){
-                    dist[tem] = dist[now]+1;
-                    queue.offer(tem);
+            for(int next: graph.get(cur)){
+                if(dist[next]==-1){
+                    dist[next] = dist[cur]+1;
+                    queue.offer(next);
                 }
             }
         }
         
+        int[] answer = new int[sources.length];
         for(int i=0; i<sources.length; i++){
-            if(dist[sources[i]]==Integer.MAX_VALUE)
-                result[i] = -1;
-            else
-                result[i] = dist[sources[i]];
+            answer[i] = dist[sources[i]];
         }
-        
-        return result;
+        return answer;
     }
 }
