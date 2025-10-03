0dd7a777abd3620766f625334b1d51c4b3933c26
strangehoon <strangehoon2@gmail.com>
2025-10-03 23:59:07 +0900

    [level 3] Title: 입국심사, Time: 56.81 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 140 MB, 시간: 102.35 ms
+메모리: 104 MB, 시간: 56.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 20:37:56
+2025년 10월 03일 23:59:05
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,24 +1,25 @@
-import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        Long jdx = (long)n*1000000000;
-        Long idx = 0l;
-        Long result = Long.MAX_VALUE;
+        
+        long idx = 0l;
+        long jdx = 1000000000000000000l;
+        long answer = Long.MAX_VALUE;
         while(idx<=jdx){
-            Long mid = (idx+jdx)/2;
-            Long cnt = 0l;
-            for(int time : times){
-                cnt += mid/time;
+            long mid = (idx+jdx)/2;
+            long std = 0;
+            for(int i=0; i<times.length; i++){
+                std += mid/times[i];
             }
-            if(cnt<n){
+            
+            if(n<=std){
+                jdx = mid-1;
+                answer = Math.min(answer, mid);
+            }
+            else if(n > std){
                 idx = mid+1;
             }
-            else{
-                jdx = mid-1;
-                result = Math.min(result, mid);
-            }
         }
-        
-        return result;
+
+        return answer;
     }
 }
