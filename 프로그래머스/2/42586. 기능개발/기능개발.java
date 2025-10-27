253f25cf407e39261e97478985fb7b0972e60cc9
strangehoon <strangehoon2@gmail.com>
2025-10-28 00:31:31 +0900

    [level 2] Title: 기능개발, Time: 0.43 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,29 +1,33 @@
 import java.util.*;
 class Solution {
     public List<Integer> solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new LinkedList<>();
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<progresses.length; i++){
-            int tem = (100-progresses[i])/speeds[i];
+        
+        int len = progresses.length;
+        int[] arr = new int[len];
+        
+        for(int i=0; i<len; i++){
             if((100-progresses[i])%speeds[i]!=0){
-                tem++;
-            }
-            queue.offer(tem);
-        }
-        int s = queue.poll();
-        int d = 1;
-        while(!queue.isEmpty()){
-            int tem = queue.poll();
-            if(s>=tem){
-                d++;
+                arr[i] = (100-progresses[i])/speeds[i] +1;
             }
             else{
-                answer.add(d);
-                s = tem;
-                d = 1;
+                arr[i] = (100-progresses[i])/speeds[i];
             }
         }
-        answer.add(d);
+        Stack<Integer> stack = new Stack<>();
+        for(int i=len-1; i>=0; i--){
+            stack.push(arr[i]);
+        }
+        
+        List<Integer> answer = new LinkedList<>();
+        while(!stack.isEmpty()){
+            int std = stack.pop();
+            int cnt = 1;
+            while(!stack.isEmpty() && stack.peek() <=std){
+                stack.pop();
+                cnt++;
+            }
+            answer.add(cnt);
+        }
         return answer;
     }
 }
