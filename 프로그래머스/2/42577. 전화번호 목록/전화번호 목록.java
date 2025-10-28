bc8a9ead5973124af0a1ed823bddc8512711c70e
strangehoon <strangehoon2@gmail.com>
2025-10-28 15:16:37 +0900

    [level 2] Title: 전화번호 목록, Time: 291.42 ms, Memory: 201 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,22 +1,25 @@
 import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        int len = phone_book.length;
-        boolean flag = true;
-        HashSet<String> set = new HashSet<>();
-        for(int i=0; i<len; i++){
-            String str = phone_book[i];
-            set.add(str);
-        }
-
-        for(int i=0; i<len; i++){
-            String str = phone_book[i];
-            for(int j=1; j<str.length(); j++){
-                if(set.contains(str.substring(0, j))){
+        
+        Arrays.sort(phone_book, new Comparator<String>(){
+            @Override
+            public int compare(String a, String b){
+                return a.length()-b.length();
+            }
+        });
+        
+        Set<String> set = new HashSet<>();
+        for(String phone : phone_book){
+            for(int i=0; i<phone.length(); i++){
+                if(set.contains(phone.substring(0, i+1))){
                     return false;
                 }
             }
+            set.add(phone);
         }
+
         return true;
     }
 }
