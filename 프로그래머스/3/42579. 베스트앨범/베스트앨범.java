4c849012f1e1b653a40de5cacc403d2470024309
strangehoon <strangehoon2@gmail.com>
2025-10-28 17:02:44 +0900

    [level 3] Title: 베스트앨범, Time: 1.22 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,81 +1,50 @@
 import java.util.*;
+
 class Solution {
-    public class Genre implements Comparable<Genre>{
-        String name;
-        int sum;
-        
-        public Genre(String name, int sum) {
-            this.name = name;
-            this.sum = sum;
-        }
-        
-        @Override
-        public int compareTo(Genre genre){
-            return genre.sum-this.sum;
-        }
-    }
-    
-    public class Song implements Comparable<Song>{
-        int idx;
-        int num;
-        
-        public Song(int idx, int num){
-            this.idx = idx;
-            this.num = num;
-        }
-        
-        @Override
-        public int compareTo(Song song){
-            if(this.num==song.num){
-                return this.idx - song.idx;
-            }
-            else{
-                return song.num - this.num;
-            }
-        }
-    }
-    
     public List<Integer> solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> map = new HashMap<>();
+        
+        Map<String, Integer> genreToSumMap = new HashMap<>();
+        Map<String, List<int[]>> genreToPlayMap = new HashMap<>();
         for(int i=0; i<genres.length; i++){
-            map.put(genres[i], map.getOrDefault(genres[i], 0)+plays[i]);
-        }
-        List<Genre> genreList = new ArrayList<>();
-        for(String str : map.keySet()){
-            genreList.add(new Genre(str, map.get(str)));
-        }
-        Collections.sort(genreList);
-        
-        List<List<Song>> songList = new ArrayList<>();
-        for(int i=0; i<genreList.size(); i++){
-            songList.add(new ArrayList<>());
+            genreToSumMap.put(genres[i], genreToSumMap.getOrDefault(genres[i], 0)+plays[i]);
+            if(!genreToPlayMap.containsKey(genres[i]))
+                genreToPlayMap.put(genres[i], new ArrayList<>());
         }
         
-        for(int i=0; i<plays.length; i++){
-            String name = genres[i];
-            for(int j=0; j<genreList.size(); j++){
-                if(genreList.get(j).name.equals(name)){
-                    songList.get(j).add(new Song(i, plays[i]));
-                }
-            }
+        for(int i=0; i<genres.length; i++){
+            genreToPlayMap.get(genres[i]).add(new int[]{i, plays[i]});
         }
         
-        for(int i=0; i<songList.size(); i++){
-            Collections.sort(songList.get(i));
+        List<Integer> sumList = new ArrayList<>();
+        for(int sum : genreToSumMap.values()){
+            sumList.add(sum);
         }
-        
-        int idx = 0;
+        Collections.sort(sumList, Collections.reverseOrder());
+
         List<Integer> answer = new ArrayList<>();
         
-        for(int i=0; i<genreList.size(); i++){
-            for(int j=0; j<songList.get(i).size(); j++){
-                if(j==2){
-                    break;
+        for(int sum : sumList){
+            for(Map.Entry<String, Integer> entry : genreToSumMap.entrySet()){
+                List<int[]> playMapList = genreToPlayMap.get(entry.getKey());
+                if(entry.getValue()==sum){
+                    Collections.sort(playMapList, new Comparator<int[]>(){
+                        @Override
+                        public int compare(int[] arr1, int[] arr2){
+                            if(arr1[1]!=arr2[1]){
+                                return arr2[1]-arr1[1];
+                            }
+                            else{
+                                return arr1[0]-arr2[0];
+                            }
+                        }
+                    });
+                    answer.add(playMapList.get(0)[0]);
+                    if(playMapList.size()>1)
+                        answer.add(playMapList.get(1)[0]);
                 }
-                answer.add(songList.get(i).get(j).idx);
             }
         }
-        
+
         return answer;
     }
 }
