b73fee1c8567c4ba93bc7050ed35b8b7780824cd
strangehoon <strangehoon2@gmail.com>
2025-10-28 20:37:17 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,22 +1,20 @@
-// 16:10
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answers = new int[commands.length];
+        
+        int[] answer = new int[commands.length];
         for(int i=0; i<commands.length; i++){
             int[] command = commands[i];
-            int s = command[0];
-            int e = command[1];
-            int k = command[2];
+            
             List<Integer> list = new ArrayList<>();
-            for(int j=s-1; j<=e-1; j++){
+            for(int j=command[0]-1; j<=command[1]-1; j++){
                 list.add(array[j]);
             }
             Collections.sort(list);
-            answers[i] = list.get(k-1);
+            answer[i] = list.get(command[2]-1);
         }
-        return answers;
+
+        return answer;
     }
 }
-
-    
