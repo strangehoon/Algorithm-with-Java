df5f9d5c9db7b843f0a687e5ba12ab8db919f1cf
strangehoon <strangehoon2@gmail.com>
2025-10-29 04:17:09 +0900

    [level 2] Title: H-Index, Time: 0.73 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,25 +1,15 @@
+import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         
-        int h = 1000;
-        int answer = 0;
-        while(h>=0){
-            int cnt = 0;
-            for(int citation : citations){
-                if(citation>=h){
-                    cnt++;
-                }
+        Arrays.sort(citations);
+        
+        for(int i=0; i<citations.length; i++){
+            int h = citations.length-i;
+            if(h<=citations[i]){
+                return h;
             }
-            if(cnt>=h){
-                answer = h;
-                break;
-            }
-            h--;
         }
-        return answer;
+        return 0;
     }
 }
-
-
-
-
