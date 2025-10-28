4440afc60f29c4f33c7fb826cbf3ac66395ef41a
strangehoon <strangehoon2@gmail.com>
2025-10-28 21:11:40 +0900

    [level 2] Title: 가장 큰 수, Time: 241.27 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,30 +1,28 @@
 import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
-        String[] strArr = new String [numbers.length];
-        for(int i = 0; i<numbers.length; i++){
-            strArr[i] = String.valueOf(numbers[i]);
+        
+        String[] numStrArr = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++){
+            numStrArr[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(strArr, new Comparator<String>(){
+        
+        Arrays.sort(numStrArr, new Comparator<String>(){
             @Override
-            public int compare(String s1, String s2){
-                int result = 0;
-                if(s1.length()==s2.length()){
-                    result = s2.compareTo(s1);
-                }
-                else{
-                    result = (s2+s1).compareTo(s1+s2);
-                }
-                return result;
+            public int compare(String str1, String str2){
+                return (str2+str1).compareTo(str1+str2);
             }
         });
+        
         StringBuilder sb = new StringBuilder();
-        if(strArr[0].equals("0"))
-            return "0";
-
-        for(String x : strArr){
-            sb.append(x);
+        for(String str : numStrArr){
+            sb.append(str);
         }
-        return sb.toString();
+        
+        if(sb.toString().charAt(0)=='0')
+            return "0";
+        else
+            return sb.toString();
     }
 }
