aea2b4eacfe39f5f81ad27b7456be41f3ac2ff73
strangehoon <strangehoon2@gmail.com>
2025-11-03 21:28:04 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.63 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,76 +1,69 @@
 import java.util.*;
+
 class Solution {
-    public class Job implements Comparable<Job>{
+    
+    public class Job{
+        int num;
         int start;
         int time;
         
-        public Job(int start, int time){
+        public Job(int num, int start, int time){
+            this.num = num;
             this.start = start;
             this.time = time;
         }
-
-        @Override
-        public int compareTo(Job j){
-            return this.time - j.time;
-        }
     }
+    
     public int solution(int[][] jobs) {
-        PriorityQueue<Job> pq = new PriorityQueue<>();
-        for(int[] job : jobs){
-            pq.offer(new Job(job[0], job[1]));
+        
+        PriorityQueue<Job> pq1 = new PriorityQueue<>(new Comparator<>(){
+            @Override
+            public int compare(Job job1, Job job2){
+                return job1.start-job2.start;
+            }
+        });
+        
+        for(int i=0; i<jobs.length; i++){
+            pq1.offer(new Job(i, jobs[i][0], jobs[i][1]));
         }
-        int cur = 0;
-        List<Integer> result = new ArrayList<>();
-        List<Job> dummy = new ArrayList<Job>();
-        while(true){
-            Job tem = pq.poll();
-            if(tem==null){
-                if(dummy.size()==0){
-                    break;
-                }    
-                else{
-                    dummy.sort(new Comparator<Job>() {
-                        @Override
-                        public int compare(Job j1, Job j2){
-                            if(j1.start!=j2.start){
-                                return j1.start - j2.start;
-                            }
-                            else{
-                                return j1.time - j2.time;
-                            }
-                        }
-                    });
-                    Job y = dummy.get(0);
-                    cur = y.start + y.time;
-                    result.add(y.time);
-                    for(int i=0; i<dummy.size(); i++){
-                        if(i==0)
-                            continue;
-                        pq.offer(dummy.get(i));
+        
+        PriorityQueue<Job> pq2 = new PriorityQueue<>(new Comparator<>(){
+            @Override
+            public int compare(Job job1, Job job2){
+                if(job1.time==job2.time){
+                    if(job1.start==job2.start){
+                        return job1.num-job2.num;
                     }
-                    dummy.clear();   
+                    else{
+                        return job1.start-job2.start;
+                    }
+                }
+                else{
+                    return job1.time-job2.time;
                 }
             }
+        });
+        
+        int time = 0;
+        int totalTime = 0;
+        int idx = 0;
+        while(!(pq1.isEmpty() && pq2.isEmpty())){
+            
+            while(!pq1.isEmpty()&&pq1.peek().start<=time){
+                pq2.offer(pq1.poll());
+            }
+            
+            if(!pq2.isEmpty() && pq2.peek().start<=time){
+                Job curJob = pq2.poll();
+                time += curJob.time;
+                totalTime += time-curJob.start; 
+            }
             else{
-                if(tem.start>cur){
-                    dummy.add(tem);
-                }
-                else{
-                    cur += tem.time;
-                    result.add(cur-tem.start);
-                    for(Job x : dummy){
-                        pq.offer(x);
-                    }
-                    dummy.clear();    
-                }
-            }    
+                time++;
+            }
         }
-        int avg;
-        int sum = 0;
-        for(int x : result){
-            sum += x;
-        }
-        avg = sum/jobs.length;
-        return avg;
+        
+        int answer = totalTime/jobs.length;
+        return answer;
     }
 }
