28cfb147616a244f169650445a10300570abfbdf
strangehoon <strangehoon2@gmail.com>
2025-11-06 11:49:02 +0900

    [level 2] Title: 소수 찾기, Time: 2070.80 ms, Memory: 349 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,38 +1,44 @@
 import java.util.*;
-class Solution {
 
-    public boolean isPrime(int num){
-        if(num==0 || num==1)
-            return false;
+class Solution {
+    public int solution(String numbers) {
+        Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i=2; i<=Math.sqrt(num); i++){
-            if(num%i==0)
-                return false;
-        }
-        return true;
-    }
-    
-    public void dfs(char[] charArr, String numStr, boolean[] isUsed, Set<Integer> answerSet){
-        
-        int num = Integer.parseInt(numStr);
-        if(isPrime(num))
-            answerSet.add(num);
-        
-        for(int i=0; i<charArr.length; i++){
-            if(!isUsed[i]){
-                isUsed[i] = true;
-                dfs(charArr, numStr+charArr[i], isUsed, answerSet);
-                isUsed[i] = false;
+        boolean[] arr = new boolean[10000000];
+        arr[0] = true;
+        arr[1] = true;
+        for(int i=2; i<10000000; i++){
+            for(int j=i+i; j<10000000; j+=i){
+                arr[j]=true;
             }
         }
-    }
-    
-    public int solution(String numbers) {
-        char[] charArr = numbers.toCharArray();
-        boolean[] isUsed = new boolean[numbers.length()];
-        Set<Integer> answerSet = new HashSet<>();
         
-        dfs(charArr, "0", isUsed, answerSet);
-        return answerSet.size();
+        int answer = 0;
+        for(int i=2; i<10000000; i++){
+            boolean flag = true;
+            for(int j=0; j<numbers.length(); j++){
+                int tmp = numbers.charAt(j)-'0';
+                map.put(tmp, map.getOrDefault(tmp, 0)+1);
+            }
+            
+            if(!arr[i]){
+                String str = String.valueOf(i);
+                for(int j=0; j<str.length(); j++){
+                    int num = str.charAt(j)-'0';
+                    if(!map.containsKey(num) || map.get(num)<=0){
+                        flag = false;
+                        break;
+                    }
+                    else{
+                        map.put(num, map.get(num)-1);
+                    }
+                }
+                if(flag==true){
+                    answer++;
+                }
+            }
+            map.clear();
+        }
+        return answer;
     }
 }
