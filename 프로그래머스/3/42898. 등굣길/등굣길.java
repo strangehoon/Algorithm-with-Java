4ae26363a7157b61c5aa53ece6e344a022856eeb
strangehoon <strangehoon2@gmail.com>
2025-11-06 10:47:42 +0900

    [level 3] Title: 등굣길, Time: 0.61 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,22 +1,19 @@
 class Solution {
-    
-    public boolean isWater(int x, int y, int[][] puddles){
-        for(int i=0; i<puddles.length; i++){
-            if(puddles[i][0]==y && puddles[i][1]==x)
-                return true;
-        }
-        return false;
-    }
-    
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n+1][m+1];
-        dp[0][1] = 1;
+        int[][] graph = new int[n+1][m+1];
+        for(int[] puddle : puddles){
+            graph[puddle[1]][puddle[0]]=1;
+        }
         
+        int[][] dp = new int[n+1][m+1];
+        dp[1][1]=1;
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(isWater(i, j, puddles))
+                if(i==1 && j==1)
                     continue;
-                dp[i][j] = (dp[i-1][j]+dp[i][j-1])%1000000007;
+                if(graph[i][j]==0){
+                    dp[i][j] = (dp[i-1][j]+dp[i][j-1])%1000000007;
+                }
             }
         }
         return dp[n][m];
