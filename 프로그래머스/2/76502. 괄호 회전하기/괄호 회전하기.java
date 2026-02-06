4f8ca2e725fadefcf420a40550bec16d46fafe48
strangehoon <strangehoon2@gmail.com>
2026-02-06 12:43:17 +0900

    [level 2] Title: 괄호 회전하기, Time: 45.24 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,34 +1,54 @@
 import java.util.*;
+
 class Solution {
-    public int solution(String s) {
-        int cnt = 0;
-        for(int i=0; i<s.length(); i++){
-            Stack<Character> stack = new Stack<>();
-            boolean flag = true;
-            for(int j=i; j<i+s.length(); j++){
-                char ch = s.charAt(j%s.length());
-                if(ch == '[' || ch == '(' || ch=='{')
-                    stack.push(ch);
-                else{
-                    if(stack.isEmpty()){
-                        flag=false;
-                        break;
-                    }
-                    else{
-                        char tem = stack.pop();
-                        if((tem=='[' && ch==']')||(tem=='(' && ch==')')||(tem=='{' && ch=='}')) 
-                            continue;
-                        else{
-                            flag=false;
-                            break;
-                        }
-                    }
-                }
-                
+    
+    public boolean isPalindrome(String str){
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)=='(' || str.charAt(i)=='[' || str.charAt(i)=='{'){
+                stack.push(str.charAt(i));
             }
-            if(stack.isEmpty() && flag==true)
-                cnt++;
+            else if(str.charAt(i)==')'){
+                if(stack.isEmpty() || stack.peek()!='('){
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
+            }
+            else if(str.charAt(i)==']'){
+                if(stack.isEmpty() || stack.peek()!='['){
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
+            }
+            else if(str.charAt(i)=='}'){
+                if(stack.isEmpty() || stack.peek()!='{'){
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
+            }
         }
+         
+        return stack.isEmpty();
+    }
+    
+    public int solution(String s) {
+        
+        int cnt = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            String newStr = s.substring(i+1, s.length()) + s.substring(0, i+1);
+            if(isPalindrome(newStr)){
+                cnt++;
+            }
+        }
+        
         return cnt;
     }
 }
