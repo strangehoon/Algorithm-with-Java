1a3714d64d9699ae90131c48a8c6fec9c1665d87
strangehoon <strangehoon2@gmail.com>
2026-02-10 11:03:15 +0900

    [level 3] Title: 스티커 모으기(2), Time: 10.24 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,28 +1,30 @@
 import java.util.*;
+// 2026-02-09
+// 10:40 ~ 11:16
 class Solution {
     public int solution(int sticker[]) {
         
         int len = sticker.length;
+        int[] dp1 = new int[len];
+        int[] dp2 = new int[len];
+        
         if(len==1){
             return sticker[0];
         }
-        if(len==2){
-            return Math.max(sticker[0], sticker[1]);
-        }
         
-        int[] dp1 = new int[len];
-        int[] dp2 = new int[len];
         dp1[0] = sticker[0];
         dp1[1] = Math.max(sticker[0], sticker[1]);
+        
+        for(int i=2; i<len-1; i++){
+            dp1[i] = Math.max(sticker[i]+dp1[i-2], dp1[i-1]);
+        }
+        
         dp2[0] = 0;
         dp2[1] = sticker[1];
         
         for(int i=2; i<len; i++){
-            if(i!=len-1)
-                dp1[i] = Math.max(dp1[i-2]+sticker[i], dp1[i-1]);
-            dp2[i] = Math.max(dp2[i-2]+sticker[i], dp2[i-1]);
+            dp2[i] = Math.max(sticker[i]+dp2[i-2], dp2[i-1]);
         }
-        
         
         return Math.max(dp1[len-2], dp2[len-1]);
     }
