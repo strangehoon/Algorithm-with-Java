edd92deb7e2090067ab0650a21eecd247f14ffb9
strangehoon <strangehoon2@gmail.com>
2026-02-13 11:28:49 +0900

    [level 2] Title: 숫자의 표현, Time: 0.45 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,6 +1,26 @@
+import java.util.*;
+// 2026-02-13
+// 11:00 ~ 11:12
 class Solution {
     public int solution(int n) {
+        
+        int idx = 0;
+        int jdx = 0;
+        int sum = 0;
         int answer = 0;
+        while(jdx<=n){
+            if(sum>=n){
+                if(sum==n){
+                    answer++;
+                }
+                idx++;
+                sum -= idx;
+            }
+            else{
+                jdx++;
+                sum += jdx;
+            }
+        }
         return answer;
     }
 }
