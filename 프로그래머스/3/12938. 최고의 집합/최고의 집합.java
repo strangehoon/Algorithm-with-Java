958e1497e04d4c751be36a9e8002eccf55e4353b
strangehoon <strangehoon2@gmail.com>
2026-03-04 12:33:24 +0900

    [level 3] Title: 최고의 집합, Time: 0.31 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,20 +1,22 @@
 import java.util.*;
+// 2026-03-04
+// 11:55 ~ 12:10
 
 class Solution {
     public int[] solution(int n, int s) {
-        
-        if(n>s)
+        if(n>s){
             return new int[]{-1};
-        
-        int a = s/n;
-        int b = s%n;
-        
-        int[] answer = new int[n];
-        Arrays.fill(answer, a);
-        
-        for(int i=0; i<b; i++){
-            answer[n-1-i]++;
         }
+        int[] answer = new int[n];
+        int share = s/n;
+        Arrays.fill(answer, share);
+        int rest = s%n;
+        
+        int idx = n-1;
+        while(rest-->0){
+            answer[idx--]++;
+        }
+        
         return answer;
     }
 }
