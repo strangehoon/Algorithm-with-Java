512e31403993c87f2a212401add7bce8393e31d8
strangehoon <strangehoon2@gmail.com>
2026-03-06 21:28:14 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 24.23 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,26 +1,37 @@
+import java.util.*;
+// 2026-03-06
+// 18:10 ~ 18:30
+
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int len = sequence.length;
-        
-        int sum = 0;
-        int j = 0;
-        int seqLen = Integer.MAX_VALUE;
         int[] answer = new int[2];
-        for(int i=0; i<len; i++){
-            sum += sequence[i];
-            
-            while(sum > k){
-                sum -= sequence[j++];     
-            }
-            
-            if(sum==k & seqLen>i-j){
-                answer[0] = j;
-                answer[1] = i;
-                seqLen = i-j;
-            }
-        }
+        int len = Integer.MAX_VALUE;
         
+        int right = 0;
+        int left = 0;
+        int sum = sequence[right];
         
+        while(right<sequence.length){
+            if(sum<k){
+                if(right==sequence.length-1){
+                    break;
+                }
+                else{
+                    sum += sequence[++right];    
+                }
+            }
+            else if(sum==k){
+                if(len>right-left){
+                    len = right-left;
+                    answer[0] = left;
+                    answer[1] = right;
+                }
+                sum -= sequence[left++];
+            }
+            else if(sum>k){
+                sum -= sequence[left++];
+            }
+        }    
         return answer;
     }
 }
