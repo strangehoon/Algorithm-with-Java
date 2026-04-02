d1a119d0be84cc4c2cb4112f220d2719006f2efc
strangehoon <strangehoon2@gmail.com>
2026-04-02 16:04:19 +0900

    [level 2] Title: 피보나치 수, Time: 3.52 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=java) 
 
 ### 성능 요약
 
-메모리: 78.1 MB, 시간: 3.13 ms
+메모리: 88.5 MB, 시간: 3.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 13일 02:40:53
+2026년 04월 02일 16:04:14
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,11 +1,16 @@
+import java.util.*;
+// 2026-04-02
+// 03:55 ~ 03:56
+
 class Solution {
-    public long solution(int n) {
-        long[] dp = new long[n+1];
-        dp[0]=0;
-        dp[1]=1;
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        
         for(int i=2; i<=n; i++){
-            dp[i] = (dp[i-2] + dp[i-1])%1234567;
+            dp[i] = (dp[i-1] + dp[i-2])%1234567;
         }
+        
         return dp[n];
     }
 }
